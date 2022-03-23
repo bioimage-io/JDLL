237301910fcffcc59f9fae8a6a2dd6b3c533bc04
carlosuc3m <100329787@alumnos.uc3m.es>
2022-03-23 17:45:29 +0100

    Add comments and identifier to the tensor manager

--- src/main/java/org/bioimageanalysis/icy/deeplearning/TensorManager.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/TensorManager.java
@@ -16,24 +16,61 @@ public class TensorManager implements AutoCloseable{
 	 * NDManager used to create and manage NDArrays
 	 */
 	private NDManager manager;
+	/**
+	 * Unique identifier of the manager
+	 */
+	private String identifier;
 	
+	/**
+	 * Manager that is needed to create Icy Tensors
+	 */
 	private TensorManager() {
 		manager = NDManager.newBaseManager();
+		identifier = manager.getName();
 	}
 	
+	/**
+	 * Build the {@link #TensorManager()}
+	 * @return an instance of TensorManager
+	 */
 	public static TensorManager build() {
 		return new TensorManager();
 	}
 	
+	/**
+	 * Creates the actual Icy tensor from an NDArray
+	 * @param tensorName
+	 * 	the name of the tensor
+	 * @param axes
+	 * 	the axes order of the tensor in String mode
+	 * @param data
+	 * 	the actual number of the tensor in an array form
+	 * @return
+	 */
 	public Tensor createTensor(String tensorName, String axes, NDArray data) {
 		return Tensor.build(tensorName, axes, data, this);
 	}
 	
+	/**
+	 * Retrieves the NDManager used to create tensors
+	 * @return the NDManager
+	 */
 	public NDManager getManager() {
 		return manager;
 	}
+	
+	/**
+	 * Retrieves identifier of the TEnsorMAnager
+	 * @return the name or identifier of the instance
+	 */
+	public String getIdentifier() {
+		return identifier;
+	}
 
 	@Override
+	/**
+	 * Close all the tensors associated to the manager and the manager
+	 */
 	public void close() throws Exception {
 		manager.close();		
 	}
