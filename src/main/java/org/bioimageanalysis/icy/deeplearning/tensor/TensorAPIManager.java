336f1651d5dd7b121f89fda6617c398b31f904b2
carlosuc3m <100329787@alumnos.uc3m.es>
2022-04-21 22:15:07 +0200

    add class explanation

---
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/tensor/TensorAPIManager.java
@@ -0,0 +1,14 @@
+package org.bioimageanalysis.icy.deeplearning.tensor;
+
+/**
+ * Class used to convert tensors that are defined in a particular API version into the API
+ * version needed to execute the model. 
+ * This is necessary because for Deep Java Library, each API is only compatible with few 
+ * engine versions, thus to avoid conflicts, the tensor should always use the NDArray
+ * from the API version compatible with the engine version.
+ * @author Carlos Garcia Lopez de Haro
+ *
+ */
+public class TensorAPIManager {
+
+}
