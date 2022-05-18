55b92967b44535780c7142733544443465666842
carlosuc3m <100329787@alumnos.uc3m.es>
2022-05-18 19:06:50 +0200

    Create binarize transformation

--- src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/BinarizeTransformation.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/BinarizeTransformation.java
@@ -1,15 +1,12 @@
 package org.bioimageanalysis.icy.deeplearning.transformations;
 
 import org.bioimageanalysis.icy.deeplearning.tensor.Tensor;
+import org.nd4j.linalg.api.buffer.DataType;
 
 public class BinarizeTransformation extends DefaultImageTransformation {
 
 	public static final String name = "binarize";
 	private Number threshold;
-
-	public Number getThreshold() {
-		return threshold;
-	}
 
 	public void setThreshold(Number threshold) {
 		this.threshold = threshold;
@@ -23,7 +20,21 @@ public class BinarizeTransformation extends DefaultImageTransformation {
 	private Tensor tensor;
 	
 	public BinarizeTransformation(Tensor tensor) {
-		this.tensor = tensor;
+		tensor.convertToDataType(DataType.FLOAT);
+		float[] data = tensor.getDataAsNDArray().data().asFloat();
+		float thres = (float) threshold;
+		for (int i = 0; i < data.length; i ++) {
+			data[i] = 2 * (data[i] - thres) / Math.abs((data[i] - thres)) - 1;
+		}
+
+		for (int i = 0; i < data.length; i ++) {
+			float aa = data[i] - thres;
+			if (aa > 0 )
+				data[i] = 1;
+			else
+				data[i] = 0;
+		}
+		tensor.getDataAsNDArray().data().setData(data);
 	}
 	
 	public Tensor apply() {
