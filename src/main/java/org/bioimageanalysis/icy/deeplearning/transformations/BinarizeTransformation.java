edb715bb732c5bdb0217f9d75618adec17e563b2
carlosuc3m <100329787@alumnos.uc3m.es>
2022-05-18 23:48:48 +0200

    remove unused imports

--- src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/BinarizeTransformation.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/BinarizeTransformation.java
@@ -1,10 +1,8 @@
 package org.bioimageanalysis.icy.deeplearning.transformations;
 
 import java.nio.FloatBuffer;
-import java.util.Collection;
 
 import org.bioimageanalysis.icy.deeplearning.tensor.Tensor;
-import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
