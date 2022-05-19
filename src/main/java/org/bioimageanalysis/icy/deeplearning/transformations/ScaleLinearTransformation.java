5d7d060b31240f5e2c0a4a039fc49322de11cfac
carlosuc3m <100329787@alumnos.uc3m.es>
2022-05-19 12:14:10 +0200

    add all the parameters to scale linear processing

--- src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/ScaleLinearTransformation.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/ScaleLinearTransformation.java
@@ -7,21 +7,25 @@ public class ScaleLinearTransformation extends DefaultImageTransformation {
 	public static final String name = "scale_linear";
 	private Number offset;
 	private Number gain;
-
-	public Number getOffset() {
-		return offset;
+	private String axes;
+	
+	private Tensor tensor;
+	
+	public ScaleLinearTransformation(Tensor tensor) {
+		this.tensor = tensor;
 	}
+
 
 	public void setOffset(Number offset) {
 		this.offset = offset;
 	}
 
-	public Number getGain() {
-		return gain;
-	}
-
 	public void setGain(Number gain) {
 		this.gain = gain;
+	}
+	
+	public void setAxes(String axes) {
+		this.axes = axes;
 	}
 
 	@Override
@@ -29,13 +33,31 @@ public class ScaleLinearTransformation extends DefaultImageTransformation {
 		return name;
 	}
 	
-	private Tensor tensor;
+	private float getFloatVal(Number val) {
+		if (val instanceof Integer)
+			return (float) (1.0 * (int) val);
+		else if (val instanceof Float)
+			return (float) val;
+		else if (val instanceof Double)
+			return (float) val;
+		else if (val instanceof Long)
+			return (float) (1.0 * (long) val);
+		else 
+			throw new IllegalArgumentException("Type '" + val.getClass().toString() + "' of the"
+					+ " parameters for the processing '" + name + "' not supported.");
+	}
 	
-	public ScaleLinearTransformation(Tensor tensor) {
-		this.tensor = tensor;
+	private void checkCompulsoryArgs() {
+		if (gain == null || offset == null) {
+			throw new IllegalArgumentException("Error defining the processing '"
+					+ name + "'. It should at least be provided with the "
+					+ "arguments 'gain' and 'offset' in the"
+					+ " yaml file.");
+		}
 	}
 	
 	public Tensor apply() {
+		checkCompulsoryArgs();
 		return tensor;
 	}
 }
