996edcbe0031bc7cbda0c9ea8cfebbc90aef38be
carlosuc3m <100329787@alumnos.uc3m.es>
2022-07-15 16:30:01 +0200

    test commit end

--- src/main/java/org/bioimageanalysis/icy/deeplearning/exceptions/RunModelException.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/exceptions/RunModelException.java
@@ -4,7 +4,6 @@ package org.bioimageanalysis.icy.deeplearning.exceptions;
  * Exception thrown when there have been problems running a Deep Learning model
  * @author Carlos Garcia Lopez de Haro
  * 
- * 
  */
 public class RunModelException extends Exception {
 	
