47be23d3f86af684e9038eb63ca8c00ba940df2e
carlosuc3m <100329787@alumnos.uc3m.es>
2022-09-29 18:15:51 +0200

    update zero mean unit variance method to rdf.yaml

--- src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/ZeroMeanUnitVarianceTensorTransformation.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/transformations/ZeroMeanUnitVarianceTensorTransformation.java
@@ -2,10 +2,12 @@ package org.bioimageanalysis.icy.deeplearning.transformations;
 
 import org.bioimageanalysis.icy.deeplearning.tensor.Tensor;
 
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.loops.LoopBuilder;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 public class ZeroMeanUnitVarianceTensorTransformation extends AbstractTensorTransformation
@@ -78,21 +80,122 @@ public class ZeroMeanUnitVarianceTensorTransformation extends AbstractTensorTran
 	@Override
 	public < R extends RealType< R > & NativeType< R > > Tensor< FloatType > apply( final Tensor< R > input )
 	{
-		final float[] meanStd = meanStd( input );
+		checkRequiredArgs();
+		final Tensor< FloatType > output = makeOutput( input );
+		String selectedAxes = "";
+		for (String ax : input.getAxesOrderString().split("")) {
+			if (axes != null && !axes.toLowerCase().contains(ax.toLowerCase())
+					&& !ax.toLowerCase().equals("b"))
+				selectedAxes += ax;
+		}
+		if (mode == Mode.FIXED &&  (axes == null || selectedAxes.equals("") 
+				|| input.getAxesOrderString().replace("b", "").length() - selectedAxes.length() == 1)) {
+			if (meanVal == null)
+				throw new IllegalArgumentException("The 'axes' parameter is not"
+						+ " compatible with the parameters 'mean' and 'std' if 'mode' is 'fixed'."
+						+ "The parameters 'mean' and 'std' cannot be arrays with the introduced 'axes'.");
+			fixedModeGlobalMeanStd(output);
+			return output;
+		} else if (mode != Mode.FIXED && (axes == null || selectedAxes.equals("") 
+				|| input.getAxesOrderString().replace("b", "").length() - selectedAxes.length() == 1)) {
+			if (meanVal == null)
+				throw new IllegalArgumentException("The 'axes' parameter is not"
+						+ " compatible with the parameters 'mean' and 'std' if 'mode' is 'fixed'."
+						+ "The parameters 'mean' and 'std' cannot be arrays with the introduced 'axes'.");
+			notFixedModeGlobalMeanStd(output);
+			return output;
+		} else if (mode != Mode.FIXED 
+				&& input.getAxesOrderString().replace("b", "").length() - selectedAxes.length() == 2) {
+			notFixedAxesMeanStd(output, selectedAxes);
+			return output;
+		} else if (mode == Mode.FIXED 
+				&& input.getAxesOrderString().replace("b", "").length() - selectedAxes.length() == 2) {
+			fixedAxesMeanStd(output, selectedAxes);
+			return output;
+		} else {
+			//TODO allow scaling of more complex structures
+			throw new IllegalArgumentException("At the moment, only allowed scaling of planes.");
+		}
+	}
+	
+	private void fixedModeGlobalMeanStd( final Tensor< FloatType > output ) {
+
+		LoopBuilder.setImages( output.getData() )
+				.multiThreaded()
+				.forEachPixel( i -> i.set( ( i.get() - meanVal.floatValue() ) / stdVal.floatValue() ) );
+	}
+	
+	private void notFixedAxesMeanStd( final Tensor< FloatType > output, String axesOfInterest) {
+		long[] start = new long[output.getData().numDimensions()];
+		long[] dims = output.getData().dimensionsAsLongArray();
+		long[] indOfDims = new long[axesOfInterest.length()];
+		long[] sizeOfDims = new long[axesOfInterest.length()];
+		for (int i = 0; i < indOfDims.length; i ++) {
+			indOfDims[i] = output.getAxesOrderString().indexOf(axesOfInterest.split("")[i]);
+		}
+		for (int i = 0; i < sizeOfDims.length; i ++) {
+			sizeOfDims[i] = dims[(int) indOfDims[i]];
+		}
+		
+		long[][] points = getAllCombinations(sizeOfDims);
+		
+		for (long[] pp : points) {
+			for (int i = 0; i < pp.length; i ++) {
+				start[(int) indOfDims[i]] = pp[i];
+				dims[(int) indOfDims[i]] = pp[i] + 1;
+			}
+			IntervalView<FloatType> plane = Views.interval( output.getData(), start, dims );
+			final float[] meanStd = meanStd( plane );
+			final float mean = meanStd[ 0 ];
+			final float std = meanStd[ 1 ];
+			LoopBuilder.setImages( plane )
+					.multiThreaded()
+					.forEachPixel( i -> i.set( ( i.get() - mean ) / std ) );
+		}
+	}
+	
+	private void fixedAxesMeanStd( final Tensor< FloatType > output, String axesOfInterest) {
+		long[] start = new long[output.getData().numDimensions()];
+		long[] dims = output.getData().dimensionsAsLongArray();
+		long[] indOfDims = new long[axesOfInterest.length()];
+		long[] sizeOfDims = new long[axesOfInterest.length()];
+		for (int i = 0; i < indOfDims.length; i ++) {
+			indOfDims[i] = output.getAxesOrderString().indexOf(axesOfInterest.split("")[i]);
+		}
+		for (int i = 0; i < sizeOfDims.length; i ++) {
+			sizeOfDims[i] = dims[(int) indOfDims[i]];
+		}
+		
+		long[][] points = getAllCombinations(sizeOfDims);
+		int c = 0;
+		for (long[] pp : points) {
+			for (int i = 0; i < pp.length; i ++) {
+				start[(int) indOfDims[i]] = pp[i];
+				dims[(int) indOfDims[i]] = pp[i] + 1;
+			}
+			IntervalView<FloatType> plane = Views.interval( output.getData(), start, dims );
+			final float mean = (float) this.meanArr[c];
+			final float std = (float) this.stdArr[c ++ ];
+			LoopBuilder.setImages( plane )
+					.multiThreaded()
+					.forEachPixel( i -> i.set( ( i.get() - mean ) / std ) );
+		}
+	}
+	
+	private void notFixedModeGlobalMeanStd( final Tensor< FloatType > output ) {
+
+		final float[] meanStd = meanStd( output.getData() );
 		final float mean = meanStd[ 0 ];
 		final float std = meanStd[ 1 ];
-
-		final Tensor< FloatType > output = makeOutput( input );
-		LoopBuilder.setImages( input.getData(), output.getData() )
+		LoopBuilder.setImages( output.getData() )
 				.multiThreaded()
-				.forEachPixel( ( i, o ) -> o.setReal( ( i.getRealDouble() - mean ) / std ) );
-		return output;
+				.forEachPixel( i -> i.set( ( i.get() - mean ) / std ) );
 	}
 
 	@Override
 	public void applyInPlace( final Tensor< FloatType > input )
 	{
-		final float[] meanStd = meanStd( input );
+		final float[] meanStd = meanStd( input.getData() );
 		final float mean = meanStd[ 0 ];
 		final float std = meanStd[ 1 ];
 
@@ -101,12 +204,12 @@ public class ZeroMeanUnitVarianceTensorTransformation extends AbstractTensorTran
 				.forEachPixel( i -> i.set( ( i.get() - mean ) / std ) );
 	}
 
-	public static final < R extends RealType< R > & NativeType< R > > float[] meanStd( final Tensor< R > input )
+	public static float[] meanStd( final RandomAccessibleInterval< FloatType > rai )
 	{
 		// Mean.
 		double sum = 0.;
 		long n = 0;
-		for ( final R p : Views.iterable( input.getData() ) )
+		for ( final FloatType p : Views.iterable( rai ) )
 		{
 			sum += p.getRealDouble();
 			n++;
@@ -118,14 +221,36 @@ public class ZeroMeanUnitVarianceTensorTransformation extends AbstractTensorTran
 
 		// Variance.
 		double sumdx2 = 0.;
-		for ( final R p : Views.iterable( input.getData() ) )
+		for ( final FloatType p : Views.iterable( rai ) )
 		{
 			final double dx = p.getRealDouble() - mean;
 			sumdx2 += dx * dx;
 		}
-		final double variance = sumdx2 / ( n - 1 );
+		final double variance = sumdx2 /  n ;
 		final double std = Math.sqrt( variance );
 
 		return new float[] { ( float ) mean, ( float ) std };
+	}
+	
+	private static long[][] getAllCombinations(long[] arr){
+		long n = 1;
+		for (long nn : arr) n *= nn;
+		long[][] allPoints = new long[(int) n][arr.length];
+		for (int i = 0; i < n; i ++) {
+			for (int j = 0; j < arr.length; j ++) {
+				int factor = 1;
+				for (int k = 0; k < j; k ++) {
+					factor *= arr[k];
+				}
+				int auxVal = i / factor;
+				int val = auxVal % ((int) arr[j]);
+				allPoints[i][j] = val;
+			}
+		}
+		return allPoints;
+	}
+	
+	public static void main(String[] args) {
+		getAllCombinations(new long[] {6,1,4});
 	}
 }
