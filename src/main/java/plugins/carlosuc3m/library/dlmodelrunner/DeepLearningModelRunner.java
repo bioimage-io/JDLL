bd4de9b446a0140e42cce20582d68d268ab59bef
carlosuc3m <100329787@alumnos.uc3m.es>
2022-10-25 12:08:09 +0200

    add class to be a library in Icy

---
+++ src/main/java/plugins/carlosuc3m/library/dlmodelrunner/DeepLearningModelRunner.java
@@ -0,0 +1,14 @@
+
+package plugins.carlosuc3m.library.dlmodelrunner;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+/**
+ * A library that allows running Deep Learning models in Java in an agnostic way
+ * 
+ * @author Carlos Garcia Lopez de Haro
+ */
+public class DeepLearningModelRunner extends Plugin implements PluginLibrary
+{
+}
