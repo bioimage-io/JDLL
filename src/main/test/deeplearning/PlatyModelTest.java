d757fdf0ec1fa9dd25841eedc45d40efef92d694
ekaterina.moreva <ekaterina.moreva@vk.team>
2022-10-26 23:33:55 +0300

    split fixes

--- src/main/test/deeplearning/PlatyModelTest.java
+++ src/main/test/deeplearning/PlatyModelTest.java
@@ -1,5 +1,6 @@
 package deeplearning;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,9 +11,7 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import bdv.util.BdvFunctions;
-import bdv.util.BdvStackSource;
 import bdv.util.volatiles.SharedQueue;
-import bdv.viewer.Source;
 import fiji.io.n5.openers.N5S3Opener;
 import lombok.extern.slf4j.Slf4j;
 import mpicbg.spim.data.SpimData;
@@ -26,49 +25,43 @@ import net.imglib2.view.Views;
 @Slf4j
 public class PlatyModelTest {
 
+    public static final String INPUT_TENSOR_NAME = "input";
+    public static final String OUTPUT_TENSOR_NAME = "output";
+    public static final String AXES = "bxyzc";
+
     @Test
     void simpleTest() throws Exception {
         Model model = loadModel();
         SharedQueue sharedQueue = new SharedQueue(7);
         SpimData spimData = N5S3Opener.readURL("https://raw.githubusercontent.com/platybrowser/platybrowser/main/data/1.0.1/images/remote/sbem-6dpf-1-whole-raw.xml",
             sharedQueue);
-        final BdvStackSource<?> stackSource = BdvFunctions.show(spimData).get(0);
-        final Source<?> source = stackSource.getSources().get(0).getSpimSource();
-        final RandomAccessibleInterval<?> randomAccessibleInterval = source.getSource(0, 3);
+        RandomAccessibleInterval<FloatType> randomAccessibleInterval = (RandomAccessibleInterval<FloatType>)
+            spimData.getSequenceDescription().getImgLoader().getSetupImgLoader(0).getImage(0);
 
         RandomAccessibleInterval<FloatType> predictions = (RandomAccessibleInterval<FloatType>) getPredictions(model, createInput(randomAccessibleInterval)).get(0).getData();
-//        RandomAccessibleInterval< FloatType > res = new ReadOnlyCachedCellImgFactory().create(
-//            randomAccessibleInterval.dimensionsAsLongArray(),
-//            new FloatType(),
-//            predictions,
-//            ReadOnlyCachedCellImgOptions.options().cellDimensions( 256, 256, 8 )
-//        );
-//        final RandomAccessibleInterval< Volatile< FloatType > > volatilePredictions = VolatileViews.wrapAsVolatile( predictions );
-        BdvFunctions.show(predictions, "prediction");
+        BdvFunctions.show(predictions, "part predictions");
         Thread.sleep(5000);
         log.info("done all");
     }
 
     private List<Tensor> createInput(RandomAccessibleInterval<?> randomAccessibleInterval) {
-
-//        RandomAccessibleInterval<FloatType> randomAccessibleInterval = (RandomAccessibleInterval<FloatType>)
-//            spimData.getSequenceDescription().getImgLoader().getSetupImgLoader(0).getImage(0);
+        log.info("create input");
         RandomAccessibleInterval<?> crop = Views.interval(randomAccessibleInterval, new long[]{0, 0, 0}, new long[]{255, 255, 7});
         RandomAccessibleInterval<FloatType> rr = Tensor.createCopyOfRaiInWantedDataType((RandomAccessibleInterval) crop, new FloatType());
-
-        RandomAccessibleInterval<FloatType> rr2 = Views.addDimension(rr, 1, 1);
+        RandomAccessibleInterval<FloatType> rr2 = Views.addDimension(rr, 0, 0);
         rr2 = Views.moveAxis(rr2, rr.numDimensions(), 0);
-        rr2 = Views.addDimension(rr2, 1, 1);
-        Tensor<FloatType> inpTensor = Tensor.build("input", "bxyzc", rr2);
+        rr2 = Views.addDimension(rr2, 0, 0);
+        Tensor<FloatType> inpTensor = Tensor.build(INPUT_TENSOR_NAME, AXES, rr2);
         return Collections.singletonList(inpTensor);
     }
 
     private List<Tensor> getPredictions(Model model, List<Tensor> inputTensors) throws Exception {
-
         ArrayImg<FloatType, FloatArray> arr2 = ArrayImgs.floats(new long[]{1, 256, 256, 8, 1});
-        Tensor<FloatType> otpTensor = Tensor.build("output", "bxyzc", arr2);
+        Tensor<FloatType> otpTensor = Tensor.build(OUTPUT_TENSOR_NAME, AXES, arr2);
         List<Tensor> output = Collections.singletonList(otpTensor);
+        log.info("run model");
         List<Tensor> result = model.runModel(inputTensors, output);
+        log.info(Arrays.toString(result.get(0).getData().maxAsDoubleArray()));
         model.closeModel();
         return result;
     }
