63889168bd752043754bc8226cc17d6977771e11
carlosuc3m <100329787@alumnos.uc3m.es>
2022-11-21 19:10:08 +0100

    create class with important software wide classes

---
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/utils/Constants.java
@@ -0,0 +1,12 @@
+package org.bioimageanalysis.icy.deeplearning.utils;
+
+/**
+ * Class that contains important constants for the software
+ * @author Carlos Garcia Lopez de Haro
+ *
+ */
+public class Constants {
+	
+	final public static String ISSUES_LINK = "https://github.com/bioimage-io/model-runner-java/issues";
+
+}
