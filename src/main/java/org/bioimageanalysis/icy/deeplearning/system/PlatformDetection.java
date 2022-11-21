db9e4433cb9ac4dd91292b060d633df9f761fc0f
carlosuc3m <100329787@alumnos.uc3m.es>
2022-11-21 16:04:44 +0100

    improve efficiency of platform detection

--- src/main/java/org/bioimageanalysis/icy/deeplearning/system/PlatformDetection.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/system/PlatformDetection.java
@@ -7,165 +7,161 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
- * Represents the detected platform in a given system. When a new instance is
- * created it is assigned the local detected platform but it can be changed
- * using the available setters.
+ * Represents the detected platform in a given system. When a new instance is created it is assigned the local detected platform but it can be changed using the
+ * available setters.
  * 
- * @author Carlos Javier Garcia Lopez de Haro
- * @author Daniel Felipe Gonzalez Obando
+ * @author Carlos Garcia Lopez de Haro and Daniel Felipe Gonzalez Obando
  */
 public class PlatformDetection
 {
-	public static final String OS_WINDOWS = "windows";
-
-	public static final String OS_OSX = "macosx";
-
-	public static final String OS_SOLARIS = "solaris";
-
-	public static final String OS_LINUX = "linux";
-
-	public static final String ARCH_PPC = "ppc";
-
-	public static final String ARCH_X86_32 = "x86";
-
-	public static final String ARCH_X86_64 = "x86_64";
-
+    public static final String OS_WINDOWS = "Windows";
+    public static final String OS_OSX = "MacOSX";
+    public static final String OS_LINUX = "Linux";
+    public static final String ARCH_PPC64LE = "ppc64le";
+    public static final String ARCH_X86_32 = "x86";
+    public static final String ARCH_X86_64 = "x86_64";
+    public static final String ARCH_S390X = "s390x";
     // Aarch64 and arm64 are equivalent architectures
     public static final String ARCH_AARCH64 = "aarch64";
-    
     public static final String ARCH_ARM64 = "arm64";
+    public static final Map<String, String> archMap;
 
-	public static final Map< String, String > archMap;
-
-	static
-	{
-		Map< String, String > architectures = new HashMap<>();
-		architectures.put( "x86", ARCH_X86_32 );
-		architectures.put( "i386", ARCH_X86_32 );
-		architectures.put( "i486", ARCH_X86_32 );
-		architectures.put( "i586", ARCH_X86_32 );
-		architectures.put( "i686", ARCH_X86_32 );
-		architectures.put( "x86_64", ARCH_X86_64 );
-		architectures.put( "amd64", ARCH_X86_64 );
-		architectures.put( "powerpc", ARCH_PPC );
+    static
+    {
+        Map<String, String> architectures = new HashMap<>();
+        architectures.put("x86", ARCH_X86_32);
+        architectures.put("i386", ARCH_X86_32);
+        architectures.put("i486", ARCH_X86_32);
+        architectures.put("i586", ARCH_X86_32);
+        architectures.put("i686", ARCH_X86_32);
+        architectures.put("x86_64", ARCH_X86_64);
+        architectures.put("amd64", ARCH_X86_64);
+        architectures.put("ppc64le", ARCH_PPC64LE);
+        architectures.put("s390x", ARCH_S390X);
         architectures.put("arm64", ARCH_ARM64);
         architectures.put("aarch64", ARCH_ARM64);
-		archMap = Collections.unmodifiableMap( architectures );
-	}
+        archMap = Collections.unmodifiableMap(architectures);
+    }
     
     private static String DETECT_CHIP_TERMINAL_COMMAND = "sysctl -n machdep.cpu.brand_string";
-
-	private String os;
-
-	private String arch;
-	
-    private boolean rosetta = false;
+    
+    private static String PYTHON_ARCH_DETECTION_COMMAND = "\"import platform; print(platform.machine())\"";
     
     private static String UNAME_M;
 
-	/**
-	 * Creates a platform detection using the local platform.
-	 */
-	public PlatformDetection()
-	{
-		// resolve OS
-		if ( System.getProperty("os.name").toLowerCase().contains("win") )
-		{
-			this.setOs( OS_WINDOWS );
-		}
-		else if ( System.getProperty("os.name").toLowerCase().replace(" ", "").contains("macosx") )
-		{
-			this.setOs( OS_OSX );
-		}
-		else if ( System.getProperty("os.name").toLowerCase().contains("sunos") 
-        		|| System.getProperty("os.name").toLowerCase().endsWith("solaris") )
-		{
-			this.setOs( OS_SOLARIS );
-		}
-		else if ( System.getProperty("os.name").toLowerCase().contains("linux") 
-        		|| System.getProperty("os.name").toLowerCase().endsWith("ix") )
-		{
-			this.setOs( OS_LINUX );
-		}
-		else
-		{
-			throw new IllegalArgumentException( "Unknown operating system " + System.getProperty("os.name") );
-		}
+    private String os;
+    private String arch;
+    private boolean rosetta = false;
 
-		// resolve architecture
-		this.setArch( archMap.get( System.getProperty("os.arch") ) );
-		if ( this.arch == null )
-		{ 
-			throw new IllegalArgumentException( "Unknown architecture " + System.getProperty("os.arch") ); 
-		}
+    /**
+     * Creates a platform detection using the local platform.
+     */
+    public PlatformDetection()
+    {
+    }
+
+    /**
+     * @return The operating system of the platform. e.g. windows, linux, macosx, etc.
+     */
+    public String getOs()
+    {
+    	if (os != null)
+    		return os;
+        // resolve OS
+        if (System.getProperty("os.name").toLowerCase().contains("win"))
+        {
+            os = OS_WINDOWS;
+        }
+        else if (System.getProperty("os.name").toLowerCase().replace(" ", "").contains("macosx"))
+        {
+            os = OS_OSX;
+        }
+        else if (System.getProperty("os.name").toLowerCase().contains("linux") 
+        		|| System.getProperty("os.name").toLowerCase().endsWith("ix"))
+        {
+            os = OS_LINUX;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Operating system not supported by Miniconda: " + System.getProperty("os.name")
+								+ ". Only supported OS are: " + OS_WINDOWS + ", " + OS_OSX + " and " + OS_LINUX);
+        }
+        return os;
+    }
+
+    /**
+     * @return The system architecture. e.g. x86, x86_64, amd64, etc.
+     */
+    public String getArch()
+    {
+    	if (arch != null)
+    		return arch;
+        // resolve architecture
+        arch = archMap.get(System.getProperty("os.arch"));
+        if (this.arch == null)
+        {
+            throw new IllegalArgumentException("Unknown architecture " + System.getProperty("os.arch"));
+        }
         if (this.arch.equals(ARCH_X86_64) && !getOs().equals(PlatformDetection.OS_WINDOWS)) {
 			try {
 				Process proc = Runtime.getRuntime().exec(
 						new String[] {"bash", "-c", DETECT_CHIP_TERMINAL_COMMAND});
 				String txt = waitProcessExecutionAndGetOutputText(proc);
-	    		if (txt.toLowerCase().equals(ARCH_ARM64) || txt.toLowerCase().equals(ARCH_ARM64))
+	    		if (txt.toLowerCase().contains("apple m"))
 	    			rosetta = true;
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.out.println("Error checking the chip architecture with bash");
 			}
         }
-	}
-	
-	public static void main(String[] args) {
-		new PlatformDetection();
-	}
-
-	/**
-	 * @return The operating system of the platform. e.g. windows, linux,
-	 *         macosx, etc.
-	 */
-	public String getOs()
-	{
-		return os;
-	}
-
-	/**
-	 * The Operating System of this detection.
-	 * 
-	 * @param os
-	 *            The operating system to be assigned.
-	 */
-	public void setOs( String os )
-	{
-		this.os = os;
-	}
-
-	/**
-	 * @return The system architecture. e.g. x86, x86_64, amd64, etc.
-	 */
-	public String getArch()
-	{
-		return arch;
-	}
-
-	/**
-	 * Sets the system architecture of this detection.
-	 * 
-	 * @param arch
-	 *            The system architecture to be assigned.
-	 */
-	public void setArch( String arch )
-	{
-		this.arch = arch;
-	}
-
-	@Override
-	public String toString()
-	{
-
-		return os + "-" + arch;
-	}
+        return arch;
+    }
     
     public boolean isUsingRosseta() {
+    	if (arch == null)
+    		getArch();
     	return this.rosetta;
+    }
+
+    @Override
+    public String toString()
+    {
+    	if (os == null)
+    		getOs();
+    	if (arch == null)
+    		getArch();
+        return os + "-" + arch;
+    }
+    
+    public static boolean isWindows() {
+    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_WINDOWS);
+    }
+    
+    public static boolean isLinux() {
+    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_LINUX);
+    }
+    
+    public static boolean isMacOS() {
+    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_OSX);
+    }
+    
+    public static String getPythonArchDetectionCommand() {
+    	return PYTHON_ARCH_DETECTION_COMMAND;
+    }
+    
+    public static String executeUnameM() {
+    	if (UNAME_M != null)
+    		return UNAME_M;
+    	Process proc;
+		try {
+			proc = Runtime.getRuntime().exec(
+					new String[] {"bash", "-c", "uname -m"});
+			UNAME_M = archMap.get(waitProcessExecutionAndGetOutputText(proc));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return UNAME_M;
     }
 	
 	public static String waitProcessExecutionAndGetOutputText(Process proc) throws IOException {
@@ -194,42 +190,4 @@ public class PlatformDetection
 	    }
 	    return text;
 	}
-    
-    public static boolean isWindows() {
-    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_WINDOWS);
-    }
-    
-    public static boolean isWindows(PlatformDetection platform) {
-    	return platform.getOs().equals(PlatformDetection.OS_WINDOWS);
-    }
-    
-    public static boolean isLinux() {
-    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_LINUX);
-    }
-    
-    public static boolean isLinux(PlatformDetection platform) {
-    	return platform.getOs().equals(PlatformDetection.OS_LINUX);
-    }
-    
-    public static boolean isMacOS() {
-    	return new PlatformDetection().getOs().equals(PlatformDetection.OS_OSX);
-    }
-    
-    public static boolean isMacOS(PlatformDetection platform) {
-    	return platform.getOs().equals(PlatformDetection.OS_OSX);
-    }
-    
-    public static String executeUnameM() {
-    	if (UNAME_M != null)
-    		return UNAME_M;
-    	Process proc;
-		try {
-			proc = Runtime.getRuntime().exec(
-					new String[] {"bash", "-c", "uname -m"});
-			UNAME_M = archMap.get(waitProcessExecutionAndGetOutputText(proc));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return UNAME_M;
-    }
 }
