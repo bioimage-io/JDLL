0cd07ee34d9d1ed2519f53f008caf7a50f471269
carlosuc3m <100329787@alumnos.uc3m.es>
2022-11-22 17:50:14 +0100

    add link to wiki

--- src/main/java/org/bioimageanalysis/icy/deeplearning/utils/Constants.java
+++ src/main/java/org/bioimageanalysis/icy/deeplearning/utils/Constants.java
@@ -8,5 +8,7 @@ package org.bioimageanalysis.icy.deeplearning.utils;
 public class Constants {
 	
 	final public static String ISSUES_LINK = "https://github.com/bioimage-io/model-runner-java/issues";
+	// TODO update wiki link
+	final public static String WIKI_LINK = "https://github.com/bioimage-io/model-runner-java/issues";
 
 }
