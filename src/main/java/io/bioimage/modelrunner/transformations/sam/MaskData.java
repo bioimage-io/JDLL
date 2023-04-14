9bff7361b89d19fb98a4d6541f035bc001742611
carlosuc3m <100329787@alumnos.uc3m.es>
2023-04-14 09:16:59 -0700

    add method to filter masks

--- src/main/java/io/bioimage/modelrunner/transformations/sam/MaskData.java
+++ src/main/java/io/bioimage/modelrunner/transformations/sam/MaskData.java
@@ -6,7 +6,10 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import io.bioimage.modelrunner.tensor.Tensor;
+import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.numeric.real.FloatType;
 
 public class MaskData {
@@ -53,20 +56,47 @@ public class MaskData {
 		return this.stats.get(key);
 	}
 	
-	public void filter(int[] keep) {
+	public void filter(boolean[] keep) {
 		for (Entry<String, Object> entry : this.stats.entrySet()) {
 			if (entry.getValue() == null) {
 				return;
-			} else if () {
-				
+			} else if (entry.getValue() instanceof RandomAccessibleInterval) {
+				stats.put(entry.getKey(), keepPlanes((RandomAccessibleInterval<FloatType>) entry.getValue(), keep));
+			} else if (entry.getValue() instanceof Tensor) {
+				RandomAccessibleInterval rai = ((Tensor) entry.getValue()).getData();
+				Tensor nTensor = Tensor.build(((Tensor) entry.getValue()).getName(), ((Tensor) entry.getValue()).getAxesOrderString(), 
+						keepPlanes(rai, keep));
+				((Tensor) entry.getValue()).close();
+				stats.put(entry.getKey(), nTensor);
+			} else if (entry.getValue() instanceof List) {
+				List<Object> list = (List<Object>) entry.getValue();
+				List<Object> nList = new ArrayList<Object>();
+				for (int i = 0; i < list.size(); i ++) {
+					if (keep[i])
+						nList.add(list.get(i));
+				}
 			}
 		}
 	}
 	
 	public static RandomAccessibleInterval<FloatType> keepPlanes(RandomAccessibleInterval<FloatType> rai, boolean[] keep) {
 		long[] orShape = rai.dimensionsAsLongArray();
-		List<Integer> keptSlices = new ArrayList<Integer>();
-		
+		List<Long> keptSlices = new ArrayList<Long>();
+		long nSlices = 0;
+		for (long i = 0; i < keep.length; i ++) {
+			if (keep[(int) i])
+				keptSlices.add(i);
+		}
+		orShape[0] = keptSlices.size();
+		Img<FloatType> nRai = 
+				new ArrayImgFactory<>(new FloatType()).create(orShape);
+		Cursor<FloatType> cursor = nRai.cursor();
+		while (cursor.hasNext()) {
+			long[] oldPos = cursor.positionAsLongArray();
+			oldPos[0] = keptSlices.get((int) oldPos[0]);
+			cursor.get().set(rai.getAt(oldPos));
+		}
+		return nRai;
 	}
 
 }
