0d5c7ad1f5f786c6289b6b7daa3b171a1f7b21a3
carlosuc3m <100329787@alumnos.uc3m.es>
2023-06-23 17:20:51 +0200

    create a super robust child first classloader

--- src/main/java/io/bioimage/modelrunner/engine/ParentLastURLClassLoader.java
+++ src/main/java/io/bioimage/modelrunner/engine/ParentLastURLClassLoader.java
@@ -23,9 +23,13 @@
 package io.bioimage.modelrunner.engine;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Custom classloader that acts as an URL classloader that tries to load first the classes in
@@ -47,7 +51,7 @@ public class ParentLastURLClassLoader extends URLClassLoader {
 	 * Auxiliary URLClassLoader that only contains the classes that
 	 * can be read from the URLs provided in the constructor
 	 */
-	private URLClassLoader helper;
+	private ClassLoader helper;
 
     /**
      * Create a child-first/parent-last URLClassLoader
@@ -56,52 +60,155 @@ public class ParentLastURLClassLoader extends URLClassLoader {
      * @param parent
      * 	parent classloader
      */
-    public ParentLastURLClassLoader(URL[] urls, ClassLoader parent) {
-        super(urls, parent);
-        helper = new URLClassLoader(urls, null);
-    }
+		public ParentLastURLClassLoader(URL[] classpath, ClassLoader parent) {
+			super(classpath, null);
+			helper = parent;
+		}
 
-    @Override
-    /**
-     * {@inheritDoc}
-     * 
-     * For the parent-last classloader, first, if the class is already loaded, just recovers
-     * it, and if it has to laod it, it tries first to load the class from the JARs,
-     * and if it is not possible, it falls back to the parent classloader
-     */
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        // First, check if the class is already loaded
-        Class<?> loadedClass = findLoadedClass(name);
-        if (loadedClass != null) {
-        	if (name.startsWith("ai.djl"))
-        		System.out.println(name);
-            return loadedClass;
-        }
+	    @Override
+	    /**
+	     * {@inheritDoc}
+	     * 
+	     * For the parent-last classloader, first, if the class is already loaded, just recovers
+	     * it, and if it has to laod it, it tries first to load the class from the JARs,
+	     * and if it is not possible, it falls back to the parent classloader
+	     */
+		protected synchronized Class<?> loadClass(String name, boolean resolve)
+				throws ClassNotFoundException {
+			// First, check if the class has already been loaded
+			Class<?> c = findLoadedClass(name);
+			if (c == null) {
+				try {
+					// checking local
+					c = findClass(name);
+				} catch (ClassNotFoundException e) {
+					c = loadClassFromParent(name, resolve);
+				} catch(SecurityException e){
+					c = loadClassFromParent(name, resolve);
+				}
+			}
+			if (resolve)
+				resolveClass(c);
+			return c;
+		}
 
-        try {
-            // Try to load the class from the URLs of this classloader
-            Class<?> localClass = findClass(name);
-            if (resolve) {
-                resolveClass(localClass);
-            }
-            return localClass;
-        } catch (ClassNotFoundException e) {
-            // Class not found in this classloader, delegate to parent classloader
-            return super.loadClass(name, resolve);
-        }
-    }
-    
-    /**
-     * {@inheritDoc}
-     * 
-     * This custom classloader checks first if the resources can be found in any of the
-     * urls provided in the constructor
-     */
-    @Override
-    public Enumeration<URL> getResources(String name) throws IOException {
-        if (helper.getResources(name) != null && helper.getResources(name).hasMoreElements()) {
-        	return helper.getResources(name);
-        }
-        return super.getResources(name);
-    }
-}
+	    /**
+	     * Check the super classloader and the parent
+	     * This call to loadClass may eventually call findClass
+	     * again, in case the parent doesn't find anything.
+	     * @param name
+	     * 	the name of the class
+	     * @param resolve
+	     * 	If true then resolve the class
+	     * @return the loaded class
+	     * @throws ClassNotFoundException if the class is not found
+	     */
+		private Class<?> loadClassFromParent(String name, boolean resolve) throws ClassNotFoundException {
+			Class<?> c;
+			try {
+				c = super.loadClass(name, resolve);
+			} catch (ClassNotFoundException e) {
+				c = loadClassFromSystem(name);
+			} catch (SecurityException e){
+				c = loadClassFromSystem(name);
+			}
+			return c;
+		}
+
+		/**
+		 * Try to load a class from the parent classloader
+		 * @param name
+		 * 	name of the class
+		 * @return the loaded class
+		 * @throws ClassNotFoundException if the class is not found
+		 */
+		private Class<?> loadClassFromSystem(String name) throws ClassNotFoundException{
+			Class<?> c = null;
+			if (helper != null) {
+				// checking system: jvm classes, endorsed, cmd classpath,
+				// etc.
+				c = helper.loadClass(name);
+			}
+			return c;
+		}
+
+		@Override
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * The child classloader resource has a bigger priority
+		 */
+		public URL getResource(String name) {
+			URL url = findResource(name);
+			if (url == null)
+				url = super.getResource(name);
+
+			if (url == null && helper != null)
+				url = helper.getResource(name);
+
+			return url;
+		}
+	    
+	    /**
+	     * {@inheritDoc}
+	     * 
+	     * This custom classloader checks first if the resources can be found in any of the
+	     * urls provided in the constructor
+	     */
+	    @Override
+		public Enumeration<URL> getResources(String name) throws IOException {
+			/**
+			 * Similar to super, but local resources are enumerated before parent
+			 * resources
+			 */
+			Enumeration<URL> systemUrls = null;
+			if (helper != null) {
+				systemUrls = helper.getResources(name);
+			}
+			Enumeration<URL> localUrls = findResources(name);
+			Enumeration<URL> parentUrls = null;
+			if (getParent() != null) {
+				parentUrls = getParent().getResources(name);
+			}
+			final List<URL> urls = new ArrayList<URL>();
+			if (localUrls != null) {
+				while (localUrls.hasMoreElements()) {
+					URL local = localUrls.nextElement();
+					urls.add(local);
+				}
+			} else if (parentUrls != null && localUrls == null) {
+				while (parentUrls.hasMoreElements()) {
+					urls.add(parentUrls.nextElement());
+				}
+			} else if (systemUrls != null && localUrls == null && parentUrls == null) {
+				while (systemUrls.hasMoreElements()) {
+					urls.add(systemUrls.nextElement());
+				}
+			}
+			return new Enumeration<URL>() {
+				Iterator<URL> iter = urls.iterator();
+
+				public boolean hasMoreElements() {
+					return iter.hasNext();
+				}
+
+				public URL nextElement() {
+					return iter.next();
+				}
+			};
+		}
+
+	    /**
+	     * REturn the resource specified by the name.
+	     * The child resource has a bigger priority
+	     * 
+	     */
+		public InputStream getResourceAsStream(String name) {
+			URL url = getResource(name);
+			try {
+				return url != null ? url.openStream() : null;
+			} catch (IOException e) {
+			}
+			return null;
+		}
+	}
