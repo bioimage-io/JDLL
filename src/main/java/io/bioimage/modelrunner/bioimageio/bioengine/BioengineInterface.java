d41d32e34ba8f57d7e6177a33d3801d7219a8aae
carlosuc3m <100329787@alumnos.uc3m.es>
2023-07-14 18:08:30 +0200

    start organizing how inputs are sent to the bioengine

--- src/main/java/io/bioimage/modelrunner/bioimageio/bioengine/BioengineInterface.java
+++ src/main/java/io/bioimage/modelrunner/bioimageio/bioengine/BioengineInterface.java
@@ -20,11 +20,15 @@
 package io.bioimage.modelrunner.bioimageio.bioengine;
 
 import java.io.File;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.bioimageanalysis.icy.deepicy.model.bioimageio.BioEngineServer;
+
+import io.bioimage.modelrunner.bioimageio.bioengine.tensor.BioengineTensor;
 import io.bioimage.modelrunner.bioimageio.description.ModelDescriptor;
-import io.bioimage.modelrunner.bioimageio.description.TensorSpec;
 import io.bioimage.modelrunner.engine.DeepLearningEngineInterface;
 import io.bioimage.modelrunner.exceptions.LoadModelException;
 import io.bioimage.modelrunner.exceptions.RunModelException;
@@ -37,15 +41,50 @@ public class BioengineInterface implements DeepLearningEngineInterface {
 	
 	private ModelDescriptor rdf;
 	
+	Map<String, Object> kwargs = new HashMap<String, Object>();
+	
+	private String modelID;
+	
+	private String modelWeights;
+	
 	private static final String MODEL_NAME_KEY = "";
+	
+	private static final String INPUTS_KEY = "";
+	
+	private static final String ID_KEY = "";
+	
+	private static final String RDF_KEY = "";
+	
+	private static final String WW_KEY = "";
+	/**
+	 * Name of the default model used to run a model coming from the BioImage.io repo
+	 */
+	private static String DEFAULT_BMZ_MODEL_NAME = "bioengine-model-runner";
 
 	@Override
 	public void run(List<Tensor<?>> inputTensors, List<Tensor<?>> outputTensors) throws RunModelException {
-		LinkedHashMap<String, Object> kwargs = new LinkedHashMap<String, Object>();
-		if (rdf.getName().equals("cellpose-python"))
-			kwargs.put(MODEL_NAME_KEY, "cellpose-python");
-		for (TensorSpec tt : rdf.getInputTensors()) {
-			
+		
+		List<Object> inputs = new ArrayList<Object>();
+		for (Tensor<?> tt : inputTensors) {
+			inputs.add(BioengineTensor.build(tt).getAsMap());
+		}
+		if (rdf.getName().equals("cellpose-python")) {
+    		Map<String, Object> pp = new HashMap<String, Object>();
+    		pp.put("diameter", 30);
+    		inputs.add(pp);
+		}
+		if (modelID != null) {
+			Map<String, Object> auxMap = new HashMap<String, Object>();
+			auxMap.put(INPUTS_KEY, inputs);
+			auxMap.put(ID_KEY, modelID);
+			auxMap.put(RDF_KEY, true);
+			if (modelWeights != null)
+				auxMap.put(WW_KEY, modelWeights);
+			ArrayList<Object> auxList = new ArrayList<Object>();
+			auxList.add(auxMap);
+			kwargs.put(INPUTS_KEY, auxList);
+		} else {
+			kwargs.put(INPUTS_KEY, inputs);
 		}
 		
 	}
@@ -54,6 +93,24 @@ public class BioengineInterface implements DeepLearningEngineInterface {
 	public void loadModel(String modelFolder, String modelSource) throws LoadModelException {
 		try {
 			rdf = ModelDescriptor.readFromLocalFile(modelFolder + File.separator + Constants.RDF_FNAME, false);
+
+			if (rdf.getName().equals("cellpose-python"))
+				kwargs.put(MODEL_NAME_KEY, "cellpose-python");
+			else {
+
+				workaroundModelID();
+				kwargs.put(MODEL_NAME_KEY, rdf.getName());
+				if (BioEngineServer.isBioImageIoKey(modelName)) {
+					kwargs.put(serializationKey, serializationVal);
+				} else {
+					kwargs.put(decodeJsonKey, decodeJsonVal);
+				}
+			}
+			if () {
+				
+			} else {
+				
+			}
 		} catch (Exception e) {
 			throw new LoadModelException("The rdf.yaml file for "
 					+ "model at '" + modelFolder + "' cannot be read.");
@@ -68,6 +125,19 @@ public class BioengineInterface implements DeepLearningEngineInterface {
 	
 	public void addServer(String server) {
 		this.server = server;
+	}
+	
+	/** TODO 
+	 * Workaround for BioImage.io model runner. It does not work with the full version, it
+	 * only works with: major_version_/second_version
+	 */
+	private void workaroundModelID() {
+		if (modelID == null)
+			return;
+		int nSubversions = modelID.length() - modelID.replace("/", "").length();
+		if (nSubversions == 2) {
+			modelID = modelID.substring(0, modelID.lastIndexOf("/"));
+		}
 	}
 
 }
