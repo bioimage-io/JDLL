7ad969307b2b8bbc1ce75decaaf0b79be0ba22c0
carlosuc3m <100329787@alumnos.uc3m.es>
2023-09-21 16:37:00 +0200

    keep improving the stardist op

--- python/ops/stardist_inference/stardist_inference.py
+++ python/ops/stardist_inference/stardist_inference.py
@@ -2,8 +2,10 @@ import tempfile
 import warnings
 from math import ceil
 from os import PathLike
+from os import path
 from pathlib import Path
 from typing import Dict, IO, List, Optional, Tuple, Union
+from csbdeep.utils import axes_check_and_normalize, normalize, _raise
 
 import xarray as xr
 from stardist import import_bioimageio as stardist_import_bioimageio
@@ -13,8 +15,107 @@ from bioimageio.core.prediction_pipeline._combined_processing import CombinedPro
 from bioimageio.core.prediction_pipeline._measure_groups import compute_measures
 from bioimageio.core.resource_io.nodes import Model
 from bioimageio.spec.model import raw_nodes
-from bioimageio.spec.shared.common import AXIS_LETTER_TO_NAME, AXIS_NAME_TO_LETTER
 from bioimageio.spec.shared.raw_nodes import ResourceDescription as RawResourceDescription
+
+import numpy as np
+
+RDF_YAML_SUFF = 'rdf.yaml'
+RDF_YAML_SUFF_DEPR = 'model.yaml'
+
+STARDIST_OP_NAME = 'stardist_op'
+
+def stardist_prediction_2d_mine(
+    model_rdf: Union[str, PathLike, dict, IO, bytes, raw_nodes.URI, RawResourceDescription],
+    input_tensor: xr.DataArray,
+    tile: Optional[Dict[str, int]] = None,
+) -> Tuple[xr.DataArray, dict]:
+    model = None
+    if isinstance(model_rdf, str) \
+            and (model_rdf.endswith(RDF_YAML_SUFF_DEPR) or model_rdf.endswith(RDF_YAML_SUFF)) \
+            and path.exists(model_rdf):
+        import shutil
+        from csbdeep.utils import save_json
+        from stardist.models import StarDist2D, StarDist3D
+        biomodel = load_resource_description(model_rdf)
+
+        # read the stardist specific content
+        if 'stardist' not in biomodel.config:
+            raise(RuntimeError("bioimage.io model not compatible"))
+        config = biomodel.config['stardist']['config']
+        thresholds = biomodel.config['stardist']['thresholds']
+        weights = biomodel.config['stardist']['weights']
+
+        # make sure that the keras weights are in the attachments
+        weights_file = None
+        for f in biomodel.attachments.files:
+            if f.name == weights and f.exists():
+                weights_file = f
+                break
+        weights_file is not None or _raise(FileNotFoundError(f"couldn't find weights file '{weights}'"))
+
+        # save the config and threshold to json, and weights to hdf5 to enable loading as stardist model
+        # copy bioimageio files to separate sub-folder
+        outpath = Path(Path(path.dirname(model_rdf)) / STARDIST_OP_NAME)
+
+        outpath.mkdir(parents=True)
+        save_json(config, str(outpath / 'config.json'))
+        save_json(thresholds, str(outpath / 'thresholds.json'))
+        shutil.copy(str(weights_file), str(outpath / "weights_bioimageio.h5"))
+
+        model_class = (StarDist2D if config['n_dim'] == 2 else StarDist3D)
+        imported_stardist_model = model_class(None, outpath.name, basedir=str(outpath.parent))
+
+    assert isinstance(biomodel, Model)
+    if len(model.inputs) != 1:
+        raise NotImplementedError("Multiple inputs for stardist models not yet implemented")
+
+    if len(model.outputs) != 1:
+        raise NotImplementedError("Multiple outputs for stardist models not yet implemented")
+
+    # rename tensor axes to single letters to match model RDF
+    #map_axes = {k: v for k, v in AXIS_NAME_TO_LETTER.items() if k in input_tensor.dims}
+    map_axes = "byxc"
+    if map_axes:
+        input_tensor = input_tensor.rename(map_axes)
+
+    prep = CombinedProcessing.from_tensor_specs(model.inputs)
+    ipt_name = model.inputs[0].name
+    sample = {ipt_name: input_tensor}
+    computed_measures = compute_measures(prep.required_measures, sample=sample)
+    prep.apply(sample, computed_measures)
+
+    preprocessed_input = sample[ipt_name]
+    #map_axes_back = {k: v for k, v in AXIS_LETTER_TO_NAME.items() if k in preprocessed_input.dims}
+    map_axes_back = "byxc"
+    if map_axes_back:
+        preprocessed_input = preprocessed_input.rename(map_axes_back)
+
+    #input_axis_order = [AXIS_LETTER_TO_NAME.get(a, a) for a in model.inputs[0].axes]
+    input_axis_order = "byxc"
+    if tile is None:
+        n_tiles: Optional[List[int]] = None
+    else:
+        n_tiles = []
+        for a in input_axis_order:
+            t = tile[a]
+            s = preprocessed_input.sizes[a]
+            n_tiles.append(max(ceil(s / t), 1))
+
+        warnings.warn(f"translated tile {tile} to n_tiles: {n_tiles} for stardist library.")
+
+    img = preprocessed_input.transpose(*input_axis_order).to_numpy()
+    labels, polys = imported_stardist_model.predict_instances(
+        img,
+        axes="".join([{"b": "S"}.get(a[0], a[0].capitalize()) for a in model.inputs[0].axes]),
+        n_tiles=n_tiles,
+    )
+
+    if len(labels.shape) == 2:  # batch dim got squeezed
+        labels = labels[None]
+
+    output_axes_wo_channels = tuple(a for a in model.outputs[0].axes if a != "c")
+    assert output_axes_wo_channels == tuple("byx")
+    return xr.DataArray(labels, dims=output_axes_wo_channels), polys
 
 
 def stardist_prediction_2d(
@@ -79,7 +180,8 @@ def stardist_prediction_2d(
         raise NotImplementedError("Multiple outputs for stardist models not yet implemented")
 
     # rename tensor axes to single letters to match model RDF
-    map_axes = {k: v for k, v in AXIS_NAME_TO_LETTER.items() if k in input_tensor.dims}
+    #map_axes = {k: v for k, v in AXIS_NAME_TO_LETTER.items() if k in input_tensor.dims}
+    map_axes = "byxc"
     if map_axes:
         input_tensor = input_tensor.rename(map_axes)
 
@@ -90,11 +192,13 @@ def stardist_prediction_2d(
     prep.apply(sample, computed_measures)
 
     preprocessed_input = sample[ipt_name]
-    map_axes_back = {k: v for k, v in AXIS_LETTER_TO_NAME.items() if k in preprocessed_input.dims}
+    #map_axes_back = {k: v for k, v in AXIS_LETTER_TO_NAME.items() if k in preprocessed_input.dims}
+    map_axes_back = "byxc"
     if map_axes_back:
         preprocessed_input = preprocessed_input.rename(map_axes_back)
 
-    input_axis_order = [AXIS_LETTER_TO_NAME.get(a, a) for a in model.inputs[0].axes]
+    #input_axis_order = [AXIS_LETTER_TO_NAME.get(a, a) for a in model.inputs[0].axes]
+    input_axis_order = "byxc"
     if tile is None:
         n_tiles: Optional[List[int]] = None
     else:
@@ -119,3 +223,8 @@ def stardist_prediction_2d(
     output_axes_wo_channels = tuple(a for a in model.outputs[0].axes if a != "c")
     assert output_axes_wo_channels == tuple("byx")
     return xr.DataArray(labels, dims=output_axes_wo_channels), polys
+
+arr = np.zeros((1, 208, 208, 3))
+xarr = xr.DataArray(arr, dims=["b", "y", "x", "c"], name="input")
+model_path = r'C:\Users\angel\OneDrive\Documentos\pasteur\git\model-runner-java\models\StarDist H&E Nuclei Segmentation_06092023_020924\rdf.yaml'
+stardist_prediction_2d_mine(model_path, xarr)
