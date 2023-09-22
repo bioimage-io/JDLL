69bff4dd8464772054687d06254a304c9f3ff4cd
carlosuc3m <100329787@alumnos.uc3m.es>
2023-09-22 12:49:18 +0200

    keep designing the structure of the stardist OP

--- src/main/java/io/bioimage/modelrunner/runmode/ops/StardistInferJdllOp.java
+++ src/main/java/io/bioimage/modelrunner/runmode/ops/StardistInferJdllOp.java
@@ -19,7 +19,11 @@
  */
 package io.bioimage.modelrunner.runmode.ops;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
 
 import io.bioimage.modelrunner.tensor.Tensor;
 import net.imglib2.type.NativeType;
@@ -49,7 +53,14 @@ public class StardistInferJdllOp implements OpInterface {
 	
 	private static final int N_STARDIST_OUTPUTS = 2;
 	
-	public void setModel(String modelName) {
+	public void setModel(String modelName) throws IllegalArgumentException {
+		Objects.requireNonNull(modelName, "The modelName input argument cannot be null.");
+		if (new File(modelName).isFile() && !isModelFileStardist(modelName))
+			throw new IllegalArgumentException("The file selected does not correspond to "
+					+ "the rdf.yaml file of a Bioiamge.io Stardist model.");
+		else if (!isModelNameStardist(modelName))
+			throw new IllegalArgumentException("The model name provided does not correspond to a valid"
+					+ " Stardist model present in the Bioimage.io online reposritory.");
 		this.modelName = modelName;
 	}
 	
@@ -105,8 +116,55 @@ public class StardistInferJdllOp implements OpInterface {
 		return false;
 	}
 	
-	public static boolean isModelCompatible() {
-		
+	/**
+	 * Check whether a model file or a model name (model name, model ID or model nickname)
+	 * correspond to a stardist model.
+	 * The model name has to correspond to a stardist model present in the bioimage.io, for 
+	 * example: 'chatty-frog'
+	 * @param modelName
+	 * 	file path or name of a stardist model
+	 * @return true if it corresponds to a stardsit model or false otherwise
+	 */
+	public static boolean isModelCompatible(String modelName) {
+		if (modelName == null)
+			return false;
+		if (new File(modelName).isFile())
+			return isModelFileStardist(modelName);
+		else {
+			return isModelNameStardist(modelName);
+		}
+	}
+	
+	/**
+	 * Whether the rdf.yaml file corresponding to a model contains
+	 * the information to load a stardist model or not.
+	 * @param modelFile
+	 * 	rdf.yaml file that should correspond to a stardist model
+	 * @return true if the rdf.yaml represents a stardist model and false otherwise
+	 */
+	public static boolean isModelFileStardist(String modelFile) {
+		return true;
+	}
+	
+	/**
+	 * Whether the model name (in the rdf.yaml any field among: 'name', 'nickname' or 'id')
+	 * corresponds to a stardist model of the Bioiamge.io onlie repository or not
+	 * @param modelName
+	 * 	a String corresponding to any of the following fields of the rdf.yaml file of a stardist
+	 * 	model: 'name', 'nickname' or 'id'
+	 * @return true if it actually corresponds to astardist model or false otherwise
+	 */
+	public static boolean isModelNameStardist(String modelName) {
+		return true;
+	}
+	
+	/**
+	 * Returns a list containing all the model names that corresponds to 
+	 * StarDist models existing in the Bioimage.io online repository.
+	 * @return list of StarDist model names from the Bioimage.io repository
+	 */
+	public static List<String> fetchStarDistModelNamesFromBioImage(){
+		return new ArrayList<String>();
 	}
 
 }
