007e72a08fc2616b0f9a3dd5b9301df04777ede1
carlosuc3m <100329787@alumnos.uc3m.es>
2023-09-28 18:04:51 +0200

    add method with helpers to write scripts for appose

---
+++ src/main/java/io/bioimage/modelrunner/runmode/RunModeScripts.java
@@ -0,0 +1,62 @@
+package io.bioimage.modelrunner.runmode;
+
+public class RunModeScripts {
+	
+	protected static final String AXES_KEY = "axes";
+	
+	protected static final String SHAPE_KEY = "shape";
+	
+	protected static final String DATA_KEY = "data";
+	
+	protected static final String NAME_KEY = "name";
+	
+	protected static final String NP_METHOD = "convertNpIntoDic";
+	
+	protected static final String XR_METHOD = "convertXrIntoDic";
+	
+	protected static final String LIST_METHOD = "convertListIntoSupportedist";
+	
+	protected static final String DICT_METHOD = "convertDicIntoDic";
+	
+	/**
+	 * Script that contains all the methods neeed to convert python types 
+	 * into Appose supported types (primitive types and dics and lists of them)
+	 */
+	protected static final String TYPE_CONVERSION_METHODS_SCRIPT = ""
+			+ "def " + NP_METHOD + "(np_arr):" + System.lineSeparator()
+			+ "  return {\"" + DATA_KEY + "\": np_arr.flatten().tolist(), \"" + SHAPE_KEY + "\": np_arr.shape}" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "def " + XR_METHOD + "(xr_arr):" + System.lineSeparator()
+			+ "  return {\"" + DATA_KEY + "\": xr_arr.values.flatten().tolist(), \"" + SHAPE_KEY + "\": xr_arr.shape, \"" + AXES_KEY + "\": xr_arr.dims,\"" + NAME_KEY + "\": xr_arr.name}" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "def " + LIST_METHOD + "(list_ob):" + System.lineSeparator()
+			+ "  n_list = []" + System.lineSeparator()
+			+ "  for value in list_ob:" + System.lineSeparator()
+			+ "    if isinstance(value, xr.DataArray):" + System.lineSeparator()
+			+ "      n_list.append(" + XR_METHOD + "(value))" + System.lineSeparator()
+			+ "    elif isinstance(value, np.ndarray):" + System.lineSeparator()
+			+ "      n_list.append(" + NP_METHOD + "(value))" + System.lineSeparator()
+			+ "    elif isinstance(value, dict):" + System.lineSeparator()
+			+ "      n_list.append(" + DICT_METHOD + "(value))" + System.lineSeparator()
+			+ "    elif isinstance(value, list):" + System.lineSeparator()
+			+ "      n_list.append(" + LIST_METHOD + "(value))" + System.lineSeparator()
+			+ "    else:" + System.lineSeparator()
+			+ "      n_list.append(value)" + System.lineSeparator()
+			+ "  return n_list" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "def " + DICT_METHOD + "(dic):" + System.lineSeparator()
+			+ "  n_dic = {}" + System.lineSeparator()
+			+ "  for key, value in d.items():" + System.lineSeparator()
+			+ "    if isinstance(value, xr.DataArray):" + System.lineSeparator()
+			+ "      n_dic[key] = " + XR_METHOD + "(value)" + System.lineSeparator()
+			+ "    elif isinstance(value, np.ndarray):" + System.lineSeparator()
+			+ "      n_dic[key] = " + NP_METHOD + "(value)" + System.lineSeparator()
+			+ "    elif isinstance(value, dict):" + System.lineSeparator()
+			+ "      n_dic[key] = " + DICT_METHOD + "(value)" + System.lineSeparator()
+			+ "    elif isinstance(value, list):" + System.lineSeparator()
+			+ "      n_dic[key] = " + LIST_METHOD + "(value)" + System.lineSeparator()
+			+ "    else:" + System.lineSeparator()
+			+ "      n_dic[key] = value" + System.lineSeparator()
+			+ "  return n_dic";
+
+}
