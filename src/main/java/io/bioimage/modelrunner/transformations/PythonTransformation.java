4380005044b247a648004b4f62c713c2a4320391
carlosuc3m <100329787@alumnos.uc3m.es>
2023-11-01 19:40:39 +0100

    add python transformation to run arbitrary python code

--- src/main/java/io/bioimage/modelrunner/transformations/PythonTransformation.java
+++ src/main/java/io/bioimage/modelrunner/transformations/PythonTransformation.java
@@ -19,15 +19,23 @@
  */
 package io.bioimage.modelrunner.transformations;
 
-import java.util.List;
+import java.io.File;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import io.bioimage.modelrunner.runmode.RunMode;
+import io.bioimage.modelrunner.runmode.ops.GenericOp;
 import io.bioimage.modelrunner.tensor.Tensor;
-
+import io.bioimage.modelrunner.utils.YAMLUtils;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
-public class PythonTransformation extends AbstractTensorPixelTransformation
+public class PythonTransformation extends AbstractTensorTransformation
 {
 	private static String name = "python";
 	
@@ -37,7 +45,9 @@ public class PythonTransformation extends AbstractTensorPixelTransformation
 	
 	private String method;
 	
-	private List<Object> args;
+	private int nOutputs;
+	
+	private LinkedHashMap<String, Object> kwargs;
 
 	public PythonTransformation()
 	{
@@ -48,7 +58,7 @@ public class PythonTransformation extends AbstractTensorPixelTransformation
 		if (envYaml instanceof String) {
 			this.envYaml = String.valueOf(envYaml);
 		} else {
-			throw new IllegalArgumentException("'envYaml' parameter has to be either and instance of "
+			throw new IllegalArgumentException("'envYaml' parameter has to be an instance of "
 					+ String.class
 					+ ". The provided argument is an instance of: " + envYaml.getClass());
 		}
@@ -58,7 +68,7 @@ public class PythonTransformation extends AbstractTensorPixelTransformation
 		if (script instanceof String) {
 			this.script = String.valueOf(script);
 		} else {
-			throw new IllegalArgumentException("'script' parameter has to be either and instance of "
+			throw new IllegalArgumentException("'script' parameter has to be an instance of "
 					+ String.class
 					+ ". The provided argument is an instance of: " + script.getClass());
 		}
@@ -68,21 +78,63 @@ public class PythonTransformation extends AbstractTensorPixelTransformation
 		if (method instanceof String) {
 			this.method = String.valueOf(method);
 		} else {
-			throw new IllegalArgumentException("'method' parameter has to be either and instance of "
+			throw new IllegalArgumentException("'method' parameter has to be an instance of "
 					+ String.class
 					+ ". The provided argument is an instance of: " + method.getClass());
 		}
 	}
+	
+	public void setNOutputs(Object nOutputs) {
+		if (Number.class.isAssignableFrom(nOutputs.getClass()) 
+				|| (nOutputs.getClass().isPrimitive() && !String.class.isAssignableFrom(nOutputs.getClass())) ) {
+			this.nOutputs = (int) nOutputs;
+		} else {
+			throw new IllegalArgumentException("'nOutputs' parameter has to be a number"
+					+ ". The provided argument is an instance of: " + nOutputs.getClass());
+		}
+	}
+	
+	public void setKwargs(Object kwargs) {
+		if (kwargs instanceof LinkedHashMap) {
+			this.kwargs = (LinkedHashMap<String, Object>) kwargs;
+		} else {
+			throw new IllegalArgumentException("'kwargs' parameter has to be an instance of "
+					+ LinkedHashMap.class
+					+ ". The provided argument is an instance of: " + method.getClass());
+		}
+	}
 
-	public < R extends RealType< R > & NativeType< R > > Tensor< FloatType > apply( final Tensor< R > input )
+	public < R extends RealType< R > & NativeType< R > > Tensor<FloatType> apply( final Tensor< R > input )
 	{
-		super.setFloatUnitaryOperator( v -> ( float ) ( 1. / ( 1. + Math.exp( -v ) ) ) );
-		return super.apply(input);
+		String envName = (String) YAMLUtils.loadFromString(envYaml).get("name");
+		String minicondaBase = Paths.get(System.getProperty("user.home"), ".local", "share", "appose", "miniconda").toString();
+		String envPath = minicondaBase + File.separator + "envs" + File.separator + envName;
+		if (!(new File(envPath).isDirectory())) {
+				// TODO install env
+		}
+		
+		GenericOp op = GenericOp.create(envPath, this.script, this.method, this.nOutputs);
+		LinkedHashMap<String, Object> nMap = new LinkedHashMap<String, Object>();
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("ddMMYYYY_HHmmss");
+		String dateString = sdf.format(cal.getTime());
+		nMap.put("input_" + dateString, input);
+		nMap.putAll(kwargs);
+		op.setInputs(nMap);
+		
+		RunMode rm;
+		try {
+			rm = RunMode.createRunMode(op);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			return (Tensor<FloatType>) input;
+		}
+		Map<String, Object> resMap = rm.runOP();
+		return (Tensor<FloatType>) resMap.entrySet().stream()
+				.map(e -> e.getValue()).collect(Collectors.toList()).get(0);
 	}
 
 	public void applyInPlace( final Tensor< FloatType > input )
 	{
-		super.setFloatUnitaryOperator( v -> ( float ) ( 1. / ( 1. + Math.exp( -v ) ) ) );
-		super.apply(input);
 	}
 }
