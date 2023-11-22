1966f2992d9b0be1075d77afd42da0b8e359470b
carlosuc3m <100329787@alumnos.uc3m.es>
2023-11-22 13:42:40 +0100

    improve postprocessing import speed

--- python/ops/stardist_postprocessing/stardist_postprocessing.py
+++ python/ops/stardist_postprocessing/stardist_postprocessing.py
@@ -1,10 +1,29 @@
+from time import time
+t = time()
 import numpy as np
-from stardist.nms import non_maximum_suppression_sparse, non_maximum_suppression
+print("impot numpy star " + str(time() - t))
+t = time()
+import sys
+sys.path.append(r'C:\Users\angel\OneDrive\Documentos\pasteur\git\deep-icy\appose_x86_64\envs\stardist\Lib\site-packages\stardist\lib')
+from stardist2d import c_non_max_suppression_inds
+print("impot nms star " + str(time() - t))
+t = time()
 from stardist.geometry import dist_to_coord, polygons_to_label
+print("impot geometry star " + str(time() - t))
+t = time()
 from xarray import DataArray
+print("impot dataarray star " + str(time() - t))
+t = time()
 from csbdeep.data import Resizer
+print("impot csb.data star " + str(time() - t))
+t = time()
+from stardist.utils import _normalize_grid
 from csbdeep.utils import axes_check_and_normalize
+print("impot csbd.utils star " + str(time() - t))
+t = time()
 import math
+print("impot math star NEW " + str(time() - t))
+
 
 
 def stardist_postprocessing(raw_output, prob_thresh, nms_thresh, n_classes=None,
@@ -129,6 +148,172 @@ def instances_from_prediction(img_shape, prob, dist, prob_thresh, nms_thresh, gr
         res_dict.update(dict(class_prob=prob_class, class_id=class_id))
 
     return labels, res_dict
+
+
+def non_maximum_suppression(dist, prob, grid=(1,1), b=2, nms_thresh=0.5, prob_thresh=0.5,
+                            use_bbox=True, use_kdtree=True, verbose=False):
+    """Non-Maximum-Supression of 2D polygons
+
+    Retains only polygons whose overlap is smaller than nms_thresh
+
+    dist.shape = (Ny,Nx, n_rays)
+    prob.shape = (Ny,Nx)
+
+    returns the retained points, probabilities, and distances:
+
+    points, prob, dist = non_maximum_suppression(dist, prob, ....
+
+    """
+
+    # TODO: using b>0 with grid>1 can suppress small/cropped objects at the image boundary
+
+    assert prob.ndim == 2 and dist.ndim == 3  and prob.shape == dist.shape[:2]
+    dist = np.asarray(dist)
+    prob = np.asarray(prob)
+    n_rays = dist.shape[-1]
+
+    grid = _normalize_grid(grid,2)
+
+    # mask = prob > prob_thresh
+    # if b is not None and b > 0:
+    #     _mask = np.zeros_like(mask)
+    #     _mask[b:-b,b:-b] = True
+    #     mask &= _mask
+
+    mask = _ind_prob_thresh(prob, prob_thresh, b)
+    points = np.stack(np.where(mask), axis=1)
+
+    dist   = dist[mask]
+    scores = prob[mask]
+
+    # sort scores descendingly
+    ind = np.argsort(scores)[::-1]
+    dist   = dist[ind]
+    scores = scores[ind]
+    points = points[ind]
+
+    points = (points * np.array(grid).reshape((1,2)))
+
+    if verbose:
+        t = time()
+
+    inds = non_maximum_suppression_inds(dist, points.astype(np.int32, copy=False), scores=scores,
+                                        use_bbox=use_bbox, use_kdtree=use_kdtree,
+                                        thresh=nms_thresh, verbose=verbose)
+
+    if verbose:
+        print("keeping %s/%s polygons" % (np.count_nonzero(inds), len(inds)))
+        print("NMS took %.4f s" % (time() - t))
+
+    return points[inds], scores[inds], dist[inds]
+
+
+def non_maximum_suppression_sparse(dist, prob, points, b=2, nms_thresh=0.5,
+                                   use_bbox=True, use_kdtree = True, verbose=False):
+    """Non-Maximum-Supression of 2D polygons from a list of dists, probs (scores), and points
+
+    Retains only polyhedra whose overlap is smaller than nms_thresh
+
+    dist.shape = (n_polys, n_rays)
+    prob.shape = (n_polys,)
+    points.shape = (n_polys,2)
+
+    returns the retained instances
+
+    (pointsi, probi, disti, indsi)
+
+    with
+    pointsi = points[indsi] ...
+
+    """
+
+    # TODO: using b>0 with grid>1 can suppress small/cropped objects at the image boundary
+
+    dist = np.asarray(dist)
+    prob = np.asarray(prob)
+    points = np.asarray(points)
+    n_rays = dist.shape[-1]
+
+    assert dist.ndim == 2 and prob.ndim == 1 and points.ndim == 2 and \
+        points.shape[-1]==2 and len(prob) == len(dist) == len(points)
+
+    verbose and print("predicting instances with nms_thresh = {nms_thresh}".format(nms_thresh=nms_thresh), flush=True)
+
+    inds_original = np.arange(len(prob))
+    _sorted = np.argsort(prob)[::-1]
+    probi = prob[_sorted]
+    disti = dist[_sorted]
+    pointsi = points[_sorted]
+    inds_original = inds_original[_sorted]
+
+    if verbose:
+        print("non-maximum suppression...")
+        t = time()
+
+    inds = non_maximum_suppression_inds(disti, pointsi, scores=probi, thresh=nms_thresh, use_kdtree = use_kdtree, verbose=verbose)
+
+    if verbose:
+        print("keeping %s/%s polyhedra" % (np.count_nonzero(inds), len(inds)))
+        print("NMS took %.4f s" % (time() - t))
+
+    return pointsi[inds], probi[inds], disti[inds], inds_original[inds]
+
+
+def non_maximum_suppression_inds(dist, points, scores, thresh=0.5, use_bbox=True, use_kdtree = True, verbose=1):
+    """
+    Applies non maximum supression to ray-convex polygons given by dists and points
+    sorted by scores and IoU threshold
+
+    P1 will suppress P2, if IoU(P1,P2) > thresh
+
+    with IoU(P1,P2) = Ainter(P1,P2) / min(A(P1),A(P2))
+
+    i.e. the smaller thresh, the more polygons will be supressed
+
+    dist.shape = (n_poly, n_rays)
+    point.shape = (n_poly, 2)
+    score.shape = (n_poly,)
+
+    returns indices of selected polygons
+    """
+
+
+    assert dist.ndim == 2
+    assert points.ndim == 2
+
+    n_poly = dist.shape[0]
+
+    if scores is None:
+        scores = np.ones(n_poly)
+
+    assert len(scores) == n_poly
+    assert points.shape[0] == n_poly
+
+    def _prep(x, dtype):
+        return np.ascontiguousarray(x.astype(dtype, copy=False))
+
+    inds = c_non_max_suppression_inds(_prep(dist,  np.float32),
+                                      _prep(points, np.float32),
+                                      int(use_kdtree),
+                                      int(use_bbox),
+                                      int(verbose),
+                                      np.float32(thresh))
+
+    return inds
+
+
+def _ind_prob_thresh(prob, prob_thresh, b=2):
+    if b is not None and np.isscalar(b):
+        b = ((b,b),)*prob.ndim
+
+    ind_thresh = prob > prob_thresh
+    if b is not None:
+        _ind_thresh = np.zeros_like(ind_thresh)
+        ss = tuple(slice(_bs[0] if _bs[0]>0 else None,
+                         -_bs[1] if _bs[1]>0 else None)  for _bs in b)
+        _ind_thresh[ss] = True
+        ind_thresh &= _ind_thresh
+    return ind_thresh
 
 
 class StarDistPadAndCropResizer(Resizer):
