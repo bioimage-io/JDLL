7c2a68476fab29cb629226cadc402165dc30e61f
carlosuc3m <100329787@alumnos.uc3m.es>
2023-12-12 23:35:10 +0100

    finish javadoc for the class

--- src/main/java/io/bioimage/modelrunner/tensor/shm/SharedMemoryFile.java
+++ src/main/java/io/bioimage/modelrunner/tensor/shm/SharedMemoryFile.java
@@ -23,23 +23,47 @@ package io.bioimage.modelrunner.tensor.shm;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import io.bioimage.modelrunner.tensor.Tensor;
 import io.bioimage.modelrunner.numpy.DecodeNumpy;
+
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
 /**
- * Class that maps {@link Tensor} objects to the shared memory for interprocessing communication
+ * TODO get rid of this class in favour of shared memory IPC
+ * Class that maps {@link Tensor} objects to a temporal file for inter-processing communication
  * 
  * @author Carlos Garcia Lopez de Haro
  */
 public final class SharedMemoryFile
 {
+	/**
+	 * Create a .npy Numpy file from a {@link RandomAccessibleInterval}
+	 * @param <T>
+	 * 	the ImgLib2 data type of the {@link RandomAccessibleInterval}
+	 * @param fileDir
+	 * 	file path to the .npy file that is going to be created
+	 * @param rai
+	 * 	the {@link RandomAccessibleInterval} of interest
+	 * @throws FileNotFoundException if the parent directory where the file wants to be created does not exist
+	 * @throws IOException if there is any error creating the file
+	 */
 	public static <T extends RealType<T> & NativeType<T>> 
 	void buildFileFromRai(String fileDir, RandomAccessibleInterval<T> rai) throws FileNotFoundException, IOException {
 		DecodeNumpy.writeRaiToNpyFile(fileDir, rai);
 	}
 	
+	/**
+	 * Read a {@link RandomAccessibleInterval} from a .npy Numpy file created to share between processes
+	 * @param <T>
+	 * 	the ImgLib2 data type of the {@link RandomAccessibleInterval}
+	 * @param fileName
+	 * 	name of the .npy file that contains a {@link RandomAccessibleInterval}
+	 * @return a {@link RandomAccessibleInterval}
+	 * @throws FileNotFoundException if the file is not found
+	 * @throws IOException if there is any error reading the file
+	 */
 	public static <T extends RealType<T> & NativeType<T>> 
 	RandomAccessibleInterval<T> buildRaiFromFile(String fileName) throws FileNotFoundException, IOException {
 		return DecodeNumpy.retrieveImgLib2FromNpy(fileName);
