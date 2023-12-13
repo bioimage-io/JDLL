665a467e7ff5b785718a36cbfd1f7f17467e9bf5
carlosuc3m <100329787@alumnos.uc3m.es>
2023-12-13 01:05:23 +0100

    add final java doc

--- src/main/java/io/bioimage/modelrunner/apposed/appose/Types.java
+++ src/main/java/io/bioimage/modelrunner/apposed/appose/Types.java
@@ -48,19 +48,36 @@ public final class Types {
 		// NB: Prevent instantiation of utility class.
 	}
 
+	/**
+	 * Encode a Map into a String
+	 * @param data
+	 * 	data that wants to be encoded
+	 * @return string containing the info of the data map
+	 */
 	public static String encode(Map<?, ?> data) {
 		Gson gson = new Gson();
 		return gson.toJson(data);
 	}
 
 	@SuppressWarnings("unchecked")
+	/**
+	 * Convert a json string into a map
+	 * @param json
+	 * 	json string
+	 * @return a map of with the information of the json
+	 */
 	public static Map<String, Object> decode(String json) {
 		Gson gson = new Gson();
         Type mapType = new TypeToken<HashMap<String, Object>>() {}.getType();
         return gson.fromJson(json, mapType);
 	}
 
-	/** Dumps the given exception, including stack trace, to a string. */
+	/** Dumps the given exception, including stack trace, to a string. 
+	 * 
+	 * @param t
+	 * 	the given exception {@link Throwable}
+	 * @return the String containing the whole exception trace
+	 */
 	public static String stackTrace(Throwable t) {
 		StringWriter sw = new StringWriter();
 		t.printStackTrace(new PrintWriter(sw));
