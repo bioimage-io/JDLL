f24dc4de760704b6329a37886d735a7c1e2d4773
carlosuc3m <100329787@alumnos.uc3m.es>
2024-02-05 14:02:13 +0100

    correct incorrect logic

--- src/main/java/io/bioimage/modelrunner/apposed/appose/Mamba.java
+++ src/main/java/io/bioimage/modelrunner/apposed/appose/Mamba.java
@@ -1014,7 +1014,6 @@ public class Mamba {
 	 */
 	public String getVersion() throws IOException, InterruptedException, MambaInstallException
 	{
-		checkMambaInstalled();
 		if (!installed) throw new MambaInstallException("Micromamba is not installed");
 		final List< String > cmd = getBaseCommand();
 		cmd.addAll( Arrays.asList( mambaCommand, "--version" ) );
