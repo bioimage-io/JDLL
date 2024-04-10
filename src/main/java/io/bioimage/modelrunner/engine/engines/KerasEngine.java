a1d82db27d7624342e4e61f2f14185144d167c47
carlosuc3m <100329787@alumnos.uc3m.es>
2024-04-10 13:58:31 +0200

    keep designing the first engine

--- src/main/java/io/bioimage/modelrunner/engine/engines/KerasEngine.java
+++ src/main/java/io/bioimage/modelrunner/engine/engines/KerasEngine.java
@@ -1,5 +1,6 @@
 package io.bioimage.modelrunner.engine.engines;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,40 +9,97 @@ import io.bioimage.modelrunner.apposed.appose.Mamba;
 import io.bioimage.modelrunner.engine.AbstractEngine;
 
 public class KerasEngine extends AbstractEngine {
+	
+	private Mamba mamba;
+	
+	private String version;
+	
+	private boolean gpu;
+	
+	private boolean isPython;
+	
+	public static final String NAME = "keras";
 
-	private static final List<String> SUPPORTED_KERAS_VERSIONS = Arrays.stream(new String[] {}).collect(Collectors.toList());
+	private static final List<String> SUPPORTED_KERAS_GPU_VERSIONS = Arrays.stream(new String[] {}).collect(Collectors.toList());
 	private static final List<String> SUPPORTED_KERAS_VERSION_NUMBERS = Arrays.stream(new String[] {}).collect(Collectors.toList());
 	
-	private KerasEngine(String version, Boolean cpu, Boolean gpu, Boolean isPython) {
-		if (!isPython) throw new IllegalArgumentException("JDLL only has support for Keras through a Python engine.");
-		Mamba mamba = new Mamba();
-		mamba.getEnvsDir()
-	}
-
-	
-	public static KerasEngine initialize(String version, Boolean cpu, Boolean gpu, Boolean isPython) {
-		return new KerasEngine(version, cpu, gpu, isPython);
-	}
-	
-	public static String getDir(String version, boolean cpu, boolean gpu, boolean isPython) {
+	private KerasEngine(String version, boolean gpu, boolean isPython) {
 		if (!isPython) 
 			throw new IllegalArgumentException("JDLL only has support for Keras through a Python engine.");
 		if (!SUPPORTED_KERAS_VERSION_NUMBERS.contains(version))
 			throw new IllegalArgumentException("The provided Keras version is not supported by JDLL: " + version
 					+ ". The supported versions are: " + SUPPORTED_KERAS_VERSION_NUMBERS);
+		if (gpu && !SUPPORTED_KERAS_GPU_VERSIONS.contains(version))
+			throw new IllegalArgumentException("The provided Keras version has no GPU support in JDLL: " + version
+					+ ". GPU supported versions are: " + SUPPORTED_KERAS_GPU_VERSIONS);
+		mamba = new Mamba();
+		this.isPython = isPython;
+		this.version = version;
+	}
+
+	
+	public static KerasEngine initialize(String version, boolean gpu, boolean isPython) {
+		return new KerasEngine(version, gpu, isPython);
+	}
+	
+	public static String getFolderName(String version, boolean gpu, boolean isPython) {
+		if (!isPython) 
+			throw new IllegalArgumentException("JDLL only has support for Keras through a Python engine.");
 		if (!SUPPORTED_KERAS_VERSION_NUMBERS.contains(version))
+			throw new IllegalArgumentException("The provided Keras version is not supported by JDLL: " + version
+					+ ". The supported versions are: " + SUPPORTED_KERAS_VERSION_NUMBERS);
+		if (gpu && !SUPPORTED_KERAS_GPU_VERSIONS.contains(version))
+			throw new IllegalArgumentException("The provided Keras version has no GPU support in JDLL: " + version
+					+ ". GPU supported versions are: " + SUPPORTED_KERAS_GPU_VERSIONS);
+		return NAME + "_" + version + (gpu ? "_gpu" : "");
+	}
+	
+	public static List<KerasEngine> getInstalledVersions() {
+		return null;
 	}
 	
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return NAME;
 	}
 	
 	@Override
 	public String getDir() {
+		return mamba.getEnvsDir() + File.separator + getFolderName(version, gpu, false);
+	}
+
+
+	@Override
+	public boolean isPython() {
+		return isPython;
+	}
+
+
+	@Override
+	public String getVersion() {
+		return version;
+	}
+
+
+	@Override
+	public boolean supportsGPU() {
+		return gpu;
+	}
+
+
+	@Override
+	public boolean isInstalled() {
+		if (!(new File(getDir()).exists()))
+			return false;
+		return getInstalledVersions().stream()
+				.filter(vv -> vv.gpu == gpu && vv.version.equals(version)).findFirst().orElse(null) != null;
+	}
+
+
+	@Override
+	public void install() {
 		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 }
