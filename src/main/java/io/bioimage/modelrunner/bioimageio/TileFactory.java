61ac43e5662c4566a4e2c274a937432060af3e8c
carlosuc3m <100329787@alumnos.uc3m.es>
2024-08-01 18:30:11 +0200

    keep fighting with this

--- src/main/java/io/bioimage/modelrunner/bioimageio/TileFactory.java
+++ src/main/java/io/bioimage/modelrunner/bioimageio/TileFactory.java
@@ -2,10 +2,13 @@ package io.bioimage.modelrunner.bioimageio;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import io.bioimage.modelrunner.bioimageio.description.ModelDescriptor;
@@ -65,9 +68,6 @@ public class TileFactory {
 	}
 	
 	public List<ImageInfo> getOptimalTileSize(List<ImageInfo> inputInfo) {
-		if (this.descriptor.getInputTensors().size() != inputInfo.size())
-			throw new IllegalArgumentException("In order to calculate the optimal patch size, "
-					+ "image dimensions for every input tensor need to be provided");
 		boolean tiling = this.descriptor.isTilingAllowed();
 		List<ImageInfo> firstIterationInputs = new ArrayList<ImageInfo>();
 		for (TensorSpec tt : this.descriptor.getInputTensors()) {
@@ -92,56 +92,114 @@ public class TileFactory {
 		}).collect(Collectors.toList());
 
 		List<ImageInfo> secondIterationInputs = new ArrayList<ImageInfo>();
-		List<Long> totPixels = new ArrayList<Long>();
 		for (int i = 0; i < firstIterationInputs.size(); i ++) {
 			TensorSpec tensor = descriptor.findInputTensor(firstIterationInputs.get(i).getTensorName());
 			if (Arrays.stream(tensor.getTileStepArr()).allMatch(ii -> ii == 0)) {
 				secondIterationInputs.add(firstIterationInputs.get(i));
 			}
-			totPixels.add(Arrays.stream(firstIterationInputs.get(i).getDimensions()).reduce(1, (x, y) -> x * y));
 		}
 		if (firstIterationInputs.size() == firstIterationInputs.size())
 			return secondIterationInputs;
 		
 		
-		List<Long> outputTotByteSizes = calculateByteSizeOfAffectedOutput(null, null, affectedTensors);
+		List<Long> outputTotByteSizes = calculateByteSizeOfAffectedOutput(affectedTensors, null, null);
 		return checkOutputSize(firstIterationInputs, affectedTensors, outputTotByteSizes);
 	}
 	
 	
-	private List<ImageInfo> checkOutputSize(List<ImageInfo> inputs, List<TensorSpec> affected, List<Long> byteSizes) {
+	private List<ImageInfo> checkOutputSize(List<ImageInfo> inputs, List<TensorSpec> affected, List<Long> outByteSizes) {
+		List<Long> totInPixels = inputs.stream().map(in -> Arrays.stream(in.getDimensions()).reduce(1, (x, y) -> x * y)).collect(Collectors.toList());
+			
 		
-		if (totPix < OPTIMAL_MAX_NUMBER_PIXELS 
-				&& outputTotByteSizes.stream().filter(oo -> oo > Integer.MAX_VALUE).findFirst().orElse(null) == null)
-			return patch;
-		
-		long minPix = 1;
-		for (int ii : tensor.getMinTileSizeArr()) minPix *= (long) ii;
-		if (minPix > OPTIMAL_MAX_NUMBER_PIXELS)
-			return Arrays.stream(min).mapToLong(i -> i).toArray();
+		if (totInPixels.stream().filter(oo -> oo > OPTIMAL_MAX_NUMBER_PIXELS).findFirst().orElse(null) == null 
+				&& outByteSizes.stream().filter(oo -> oo > Integer.MAX_VALUE).findFirst().orElse(null) == null)
+			return inputs;
 
-		double ratioSize = (double) OPTIMAL_MAX_NUMBER_PIXELS / (double) totPix;
-		double ratioByte = (double) Integer.MAX_VALUE / (double) outputTotByteSize;
+
+		List<Double> inRatio = totInPixels.stream().map(ss -> (double) OPTIMAL_MAX_NUMBER_PIXELS / (double) ss).collect(Collectors.toList());
+		List<Double> outRatio = outByteSizes.stream().map(ss -> (double) Integer.MAX_VALUE / (double) ss).collect(Collectors.toList());
 		
-		double ratio = Math.min(ratioSize, ratioByte);
+		while(Collections.min(inRatio) < 1) {
+			Integer argmin = null;
+			List<Integer> sortedIndices = IntStream.range(0, inRatio.size())
+				    .boxed()
+				    .sorted(Comparator.comparing(inRatio::get))
+				    .collect(Collectors.toList());
+			for (Integer ind : sortedIndices) {
+				TensorSpec tt = this.descriptor.findInputTensor(inputs.get(ind).getTensorName());
+				if (Arrays.stream(tt.getTileStepArr()).allMatch(ii -> ii == 0))
+					continue;
+				argmin = ind;
+				break;
+			}
+			if (argmin == null) break;
+			
+			Double startingRatio = inRatio.get(argmin);
+			ImageInfo in = inputs.get(argmin);
+			long[] dims = in.getDimensions();
+			TensorSpec tt = this.descriptor.findInputTensor(in.getTensorName());
+			int c = 0;
+			for (String ax : in.getAxesOrder().split("")) {
+				Axis axis = tt.getAxesInfo().getAxis(ax);
+				if (axis.getStep() == 0) continue;
+				long nTot = totInPixels.get(argmin) / in.getDimensions()[c];
+				if ((in.getDimensions()[c] * inRatio.get(argmin) < axis.getMin()) && (axis.getMin() > 1)) {
+					in.getDimensions()[c] = (int)Math.ceil((double) 100 / (double) axis.getStep()) * axis.getStep();
+				} else if (in.getDimensions()[c] * inRatio.get(argmin) < axis.getMin()) {
+					in.getDimensions()[c] = axis.getMin();
+				} else {
+					in.getDimensions()[c] = (long) (Math.floor((in.getDimensions()[c] * inRatio.get(argmin) - axis.getMin()) / axis.getStep()) * axis.getStep() + axis.getMin());
+				}
+				totInPixels.set(argmin, nTot * in.getDimensions()[c]);
+				inRatio = totInPixels.stream().map(ss -> (double) OPTIMAL_MAX_NUMBER_PIXELS / (double) ss).collect(Collectors.toList());
+				
+				if (startingRatio == inRatio.get(argmin))
+					break;
+			}
+		}
 		
-		for (int ii = 0; ii < axesArr.length; ii ++) {
-			if (step[ii] == 0) continue;
-			long prevTile = patch[ii];
-			long nTot = totPix / prevTile;
-			if ((prevTile * ratio < min[ii]) && (min[ii] < 100) && (min[ii] != 1) && (min[ii] != 0))
-				patch[ii] = (int)Math.ceil((double)100 / (double)step[ii]) * step[ii];
-			else if (prevTile * ratio < min[ii])
-				patch[ii] = min[ii];
-			else 
-				patch[ii] = (long) (Math.floor((prevTile * ratio - min[ii]) / step[ii]) * step[ii] + min[ii]);
-			totPix = nTot * patch[ii];
-			ratioSize = (double) OPTIMAL_MAX_NUMBER_PIXELS / (double) totPix;
-			ratioByte = (double) Integer.MAX_VALUE / (double) calculateByteSizeOfAffectedOutput(tensor.getAxesOrder(), patch, affectedTensors);
-			ratio = Math.min(ratioSize, ratioByte);
-			if (ratio > 1)
+		outByteSizes = calculateByteSizeOfAffectedOutput(affected, null, null);
+		outRatio = outByteSizes.stream().map(ss -> (double) Integer.MAX_VALUE / (double) ss).collect(Collectors.toList());
+		
+		if (Collections.min(outRatio) < 1 && Collections.min(inRatio) < 1 )
+			throw new IllegalArgumentException("The input and/or ouput dimensions of the tensors specified by the current model are to big. JDLL is not able to run them.");
+		
+		while(Collections.min(outRatio) < 1) {
+			final List<Double> finalOutRatio = new ArrayList<>(outRatio);
+			int argmin = IntStream.range(0, finalOutRatio.size())
+			    .reduce((i, j) -> finalOutRatio.get(i) < finalOutRatio.get(j) ? i : j)
+			    .getAsInt();
+			Double oldRatio = outRatio.get(argmin);
+			
+			TensorSpec tt = this.descriptor.getOutputTensors().get(argmin);
+			for (Axis ax : tt.getAxesInfo().getAxesList()) {
+				if (ax.getReferenceTensor() == null)
+					continue;
+				TensorSpec inputT = this.descriptor.findInputTensor(ax.getReferenceTensor());
+				ImageInfo im = inputs.stream().filter(in -> in.getTensorName().equals(inputT.getTensorID())).findFirst().orElse(null);
+				String refAxis = ax.getReferenceAxis();
+				int index = im.getAxesOrder().indexOf(refAxis);
+				Axis inAx = inputT.getAxesInfo().getAxis(refAxis);
+				long size = im.getDimensions()[index];
+				
+				if ((size * outRatio.get(argmin) < inAx.getMin()) && (inAx.getMin() > 1)) {
+					im.getDimensions()[index] = (int)Math.ceil((double) 100 / (double) inAx.getStep()) * inAx.getStep();
+				} else if (size * outRatio.get(argmin) < inAx.getMin()) {
+					im.getDimensions()[index] = inAx.getMin();
+				} else {
+					im.getDimensions()[index] = (long) (Math.floor((size * outRatio.get(argmin) - inAx.getMin()) / inAx.getStep()) * inAx.getStep() + inAx.getMin());
+				}
+				double change = (size * ax.getScale() + 2 * ax.getOffset()) / (im.getDimensions()[index] * ax.getScale() + 2 * ax.getOffset());
+				outRatio.set(argmin, outRatio.get(argmin) * change);
+				if (outRatio.get(argmin) > 1)
+					break;
+			}
+			if (outRatio.get(argmin) == oldRatio)
 				break;
 		}
+		
+		if (Collections.min(outRatio) < 1)
+			throw new IllegalArgumentException("Due to the model specifications, the size of one of the output tensors exceeds the limit of tensor size in JDLL: " + Integer.MAX_VALUE);
 		return null;
 	}
 	
@@ -158,7 +216,7 @@ public class TileFactory {
     		return LongStream.range(0, affectedOutputs.size()).map(i -> 0L).boxed().collect(Collectors.toList());
     	List<String> names = inputTensors.stream().map(t -> t.getTensorID()).collect(Collectors.toList());
     	List<String> axesOrders = inputTensors.stream().map(t -> t.getAxesOrder()).collect(Collectors.toList());
-    	List<TensorSpecV05> outputTensors = null; // TODO
+    	List<TensorSpec> outputTensors = this.descriptor.getOutputTensors();
     	outputTensors = outputTensors.stream()
     			.filter(t -> {
     				return t.getAxesInfo().getAxesList().stream()
@@ -169,7 +227,7 @@ public class TileFactory {
     			.map(t -> new long[t.getAxesInfo().getAxesList().size()]).collect(Collectors.toList());
     	
     	for (int i = 0; i < outputTensors.size(); i ++) {
-    		TensorSpecV05 tt = outputTensors.get(i);
+    		TensorSpec tt = outputTensors.get(i);
     		for (int j = 0; j < outputTensors.get(i).getAxesInfo().getAxesList().size(); j ++) {
     			Axis ax = tt.getAxesInfo().getAxesList().get(j);
     			if (ax.getStep() == 0) {
@@ -178,7 +236,7 @@ public class TileFactory {
     			}
     			String refName = ax.getReferenceTensor();
     			String refAxisStr = ax.getReferenceAxis();
-    			TensorSpecV05 refTensor = inputTensors.get(names.indexOf(refName));
+    			TensorSpec refTensor = inputTensors.get(names.indexOf(refName));
     			long[] refTileSize = inputSize.get(names.indexOf(refName));
     			String axesOrder = axesOrders.get(names.indexOf(refName));
     			Axis refAxis = refTensor.getAxesInfo().getAxis(refAxisStr);
