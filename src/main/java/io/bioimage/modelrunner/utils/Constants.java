8d919c96f265ca6658c71f55246e3f3a1df5dd3e
carlosuc3m <100329787@alumnos.uc3m.es>
2024-09-21 13:30:45 +0200

    update links

--- src/main/java/io/bioimage/modelrunner/utils/Constants.java
+++ src/main/java/io/bioimage/modelrunner/utils/Constants.java
@@ -31,9 +31,9 @@ import io.bioimage.modelrunner.transformations.PythonTransformation;
  */
 public class Constants {
 	
-	final public static String ISSUES_LINK = "https://github.com/bioimage-io/model-runner-java/issues";
+	final public static String ISSUES_LINK = "https://github.com/bioimage-io/JDLL/issues";
 	// TODO update wiki link
-	final public static String WIKI_LINK = "https://github.com/bioimage-io/model-runner-java/issues";
+	final public static String WIKI_LINK = "https://github.com/bioimage-io/JDLL/wiki";
 	final public static String ENGINES_LINK = "https://raw.githubusercontent.com/bioimage-io/JDLL/main/src/main/resources/availableDLVersions.yml";
 
 	/**
