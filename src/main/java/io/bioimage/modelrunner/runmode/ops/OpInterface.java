7ea0a4c973226a4190cd035dfd97bb96088a2dcd
carlosuc3m <100329787@alumnos.uc3m.es>
2024-11-20 18:26:12 +0100

    update license headers

--- src/main/java/io/bioimage/modelrunner/runmode/ops/OpInterface.java
+++ src/main/java/io/bioimage/modelrunner/runmode/ops/OpInterface.java
@@ -2,7 +2,7 @@
  * #%L
  * Use deep learning frameworks from Java in an agnostic and isolated way.
  * %%
- * Copyright (C) 2022 - 2023 Institut Pasteur and BioImage.IO developers.
+ * Copyright (C) 2022 - 2024 Institut Pasteur and BioImage.IO developers.
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
