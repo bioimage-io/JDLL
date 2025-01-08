ea99953c2f3e30bd706f97a43945587644d9cd7e
carlosuc3m <100329787@alumnos.uc3m.es>
2025-01-08 13:03:33 +0100

    use a predefined dictionary for jar sizes

---
+++ src/main/java/io/bioimage/modelrunner/versionmanagement/JarInfo.java
@@ -0,0 +1,63 @@
+package io.bioimage.modelrunner.versionmanagement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+public class JarInfo {
+
+    // Static instance for the Singleton
+    private static JarInfo instance;
+
+    // Map to store the parsed JSON data
+    private Map<String, Long> urlData;
+        
+    private static URL FILE_PATH = JarInfo.class.getClassLoader().getResource("jar_sizes.json");
+
+    // Private constructor to restrict instantiation
+    private JarInfo() throws IOException {
+        loadJsonData();
+    }
+
+    /**
+     * Public method to initialize or get the singleton instance.
+     *
+     * @return The single instance of {@link JarInfo}
+     * @throws IOException If the file cannot be loaded
+     */
+    public static JarInfo getInstance() throws IOException {
+        if (instance == null) {
+            instance = new JarInfo();
+        }
+        return instance;
+    }
+
+    /**
+     * Public method to access the URL data
+     *
+     * @return Map containing the URL and their respective sizes
+     */
+    public Map<String, Long> getUrlData() {
+        return urlData;
+    }
+
+    /**
+     * Method to retrieve the size for a specific URL
+     *
+     * @param url The URL to look up
+     * @return The size associated with the URL, or null if not found
+     */
+    public Long getSizeForUrl(String url) {
+        return urlData.get(url);
+    }
+
+    // Private method to load and parse the JSON data
+    @SuppressWarnings("unchecked")
+	private void loadJsonData() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        urlData = objectMapper.readValue(FILE_PATH, Map.class);
+    }
+}
+
