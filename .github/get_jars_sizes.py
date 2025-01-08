fd05ff78140426bd76898e12ba46d63c3b8a11fc
carlosuc3m <100329787@alumnos.uc3m.es>
2025-01-08 11:50:04 +0100

    correct errors

--- .github/get_jars_sizes.py
+++ .github/get_jars_sizes.py
@@ -1,11 +1,20 @@
 import json
 import requests
+import argparse
 
 def get_file_size(url):
     response = requests.head(url, allow_redirects=True)
     if 'Content-Length' not in response.headers:
         raise Exception(f"Unable to find size of {url}")
     return int(response.headers['Content-Length'])
+    
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Get file sizes and save them to a target file.")
+parser.add_argument('target_file', type=str, help="Path to the target file where results will be saved.")
+args = parser.parse_args()
+
+# Assign the target file from the command-line argument
+target_file = args.target_file
 
 
 fname="src/main/resources/availableDLVersions.json"
@@ -20,3 +29,7 @@ for engine in engines["versions"]:
 		if jar in jar_dict.keys:
 			continue
 		jar_dict[jar] = get_file_size(jar)
+		
+		
+with open(target_file, "w") as ff:
+	json.dump(jar_dict)
