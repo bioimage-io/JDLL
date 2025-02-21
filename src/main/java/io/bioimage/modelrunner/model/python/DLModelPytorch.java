d43ff0af39432a9cb6d48a2d7b4e5d33ae87eaed
carlosuc3m <100329787@alumnos.uc3m.es>
2025-02-21 16:47:33 +0100

    basic functions of dl model pytorch

--- src/main/java/io/bioimage/modelrunner/model/python/DLModelPytorch.java
+++ src/main/java/io/bioimage/modelrunner/model/python/DLModelPytorch.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -24,12 +25,15 @@ import io.bioimage.modelrunner.apposed.appose.Types;
 import io.bioimage.modelrunner.exceptions.LoadModelException;
 import io.bioimage.modelrunner.exceptions.RunModelException;
 import io.bioimage.modelrunner.model.BaseModel;
+import io.bioimage.modelrunner.system.PlatformDetection;
 import io.bioimage.modelrunner.tensor.Tensor;
 import io.bioimage.modelrunner.tensor.shm.SharedMemoryArray;
 import io.bioimage.modelrunner.utils.CommonUtils;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Cast;
+import net.imglib2.util.Util;
 
 public class DLModelPytorch extends BaseModel {
 	
@@ -46,6 +50,12 @@ public class DLModelPytorch extends BaseModel {
 	private Service python;
 	
 	private List<SharedMemoryArray> inShmaList = new ArrayList<SharedMemoryArray>();
+		
+	private List<String> outShmNames;
+	
+	private List<String> outShmDTypes;
+	
+	private List<long[]> outShmDims;
 	
 	public static final String COMMON_PYTORCH_ENV_NAME = "biapy";
 	
@@ -76,6 +86,69 @@ public class DLModelPytorch extends BaseModel {
 			+ "  globals()['shared_memory'] = shared_memory" + System.lineSeparator()
 			+ "sys.path.append(os.path.abspath('%s'))" + System.lineSeparator()
 			+ "from %s import %s" + System.lineSeparator();
+	
+	private static final String OUTPUT_LIST_KEY = "out_list" + UUID.randomUUID().toString();
+	
+	private static final String SHMS_KEY = "shms_" + UUID.randomUUID().toString();
+	
+	private static final String SHM_NAMES_KEY = "shm_names_" + UUID.randomUUID().toString();
+	
+	private static final String DTYPES_KEY = "dtypes_" + UUID.randomUUID().toString();
+	
+	private static final String DIMS_KEY = "dims_" + UUID.randomUUID().toString();
+	
+	protected static final String RECOVER_OUTPUTS_CODE = ""
+			+ "def handle_output_list(out_list):" + System.lineSeparator()
+			+ "  for i, outs_i in range(out_list):" + System.lineSeparator()
+			+ "    if type(outs_i) == np.ndarray:" + System.lineSeparator()
+			+ "      shm = shared_memory.SharedMemory(create=True, size=outs_i.nbytes)" + System.lineSeparator()
+			+ "      sh_np_array = np.ndarray(outs_i.shape, dtype=outs_i.dtype, buffer=outs_i.buf)" + System.lineSeparator()
+			+ "      np.copyto(sh_np_array, outs_i)" + System.lineSeparator()
+			+ "      " + SHMS_KEY + ".append(shm)" + System.lineSeparator()
+			+ "      " + SHM_NAMES_KEY + ".append(shm.name)" + System.lineSeparator()
+			+ "      " + DTYPES_KEY + ".append(outs_i.dtype)" + System.lineSeparator()
+			+ "      " + DIMS_KEY + ".append(outs_i.shape)" + System.lineSeparator()
+			+ "    elif type(outs_i) == torch.tensor:" + System.lineSeparator()
+			+ "      shm = shared_memory.SharedMemory(create=True, size=outs_i.numel() * outs_i.element_size())" + System.lineSeparator()
+			+ "      np_arr = np.ndarray(outs_i.shape, dtype=outs_i.dtype.name, buffer=shm.buf)" + System.lineSeparator()
+			+ "      tensor_np_view = torch.from_numpy(np_arr)" + System.lineSeparator()
+			+ "      tensor_np_view.copy_(outs_i)" + System.lineSeparator()
+			+ "      " + SHMS_KEY + ".append(shm)" + System.lineSeparator()
+			+ "      " + SHM_NAMES_KEY + ".append(shm.name)" + System.lineSeparator()
+			+ "      " + DTYPES_KEY + ".append(outs_i.dtype.name)" + System.lineSeparator()
+			+ "      " + DIMS_KEY + ".append(outs_i.shape)" + System.lineSeparator()
+			+ "    elif type(outs_i) == int:" + System.lineSeparator()
+			+ "      shm = shared_memory.SharedMemory(create=True, size=8)" + System.lineSeparator()
+			+ "      shm.buf[:8] = outs_i.to_bytes(8, byteorder='little', signed=True)" + System.lineSeparator()
+			+ "      " + SHMS_KEY + ".append(shm)" + System.lineSeparator()
+			+ "      " + SHM_NAMES_KEY + ".append(shm.name)" + System.lineSeparator()
+			+ "      " + DTYPES_KEY + ".append('int64')" + System.lineSeparator()
+			+ "      " + DIMS_KEY + ".append((1))" + System.lineSeparator()
+			+ "    elif type(outs_i) == float:" + System.lineSeparator()
+			+ "      shm = shared_memory.SharedMemory(create=True, size=8)" + System.lineSeparator()
+			+ "      shm.buf[:8] = outs_i.to_bytes(8, byteorder='little', signed=True)" + System.lineSeparator()
+			+ "      " + SHMS_KEY + ".append(shm)" + System.lineSeparator()
+			+ "      " + SHM_NAMES_KEY + ".append(shm.name)" + System.lineSeparator()
+			+ "      " + DTYPES_KEY + ".append('float64')" + System.lineSeparator()
+			+ "      " + DIMS_KEY + ".append((1))" + System.lineSeparator()
+			+ "    elif type(outs_i) == tuple or type(outs_i) == list:" + System.lineSeparator()
+			+ "      handle_output_list(outs_i)" + System.lineSeparator()
+			+ "    else:" + System.lineSeparator()
+			+ "      task.update('output type : ' + str(type(outs_i)) + ' not supported. "
+			+ "Only supported output types are: np.ndarray, torch.tensor, int and float, "
+			+ "or a list or tuple of any of those.')" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "globals()['handle_output_list'] = handle_output_list" + System.lineSeparator()
+			+ "" + System.lineSeparator()
+			+ "" + System.lineSeparator();
+
+	private static final String CLEAN_SHM_CODE = ""
+			+ "if '" + SHMS_KEY + "' in globals().keys():" + System.lineSeparator()
+			+ "  for s in " + SHMS_KEY + ":" + System.lineSeparator()
+			+ "    s.close()" + System.lineSeparator()
+			+ "    s.unlink()" + System.lineSeparator()
+			+ "    del s" + System.lineSeparator();
 	
 	private DLModelPytorch(String modelFile, String callable, String weightsPath, 
 			Map<String, Object> kwargs) throws IOException {
@@ -122,6 +195,8 @@ public class DLModelPytorch extends BaseModel {
 		moduleName = moduleName.substring(0, moduleName.length() - 3);
 		String code = String.format(LOAD_MODEL_CODE_ABSTRACT, this.modelFile, moduleName, callable);
 		code += buildModelCode();
+		
+		code += RECOVER_OUTPUTS_CODE;
 
 		Task task;
 		try {
@@ -164,32 +239,230 @@ public class DLModelPytorch extends BaseModel {
 		loaded = false;
 		
 	}
+	
+	private <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> 
+	Map<String, RandomAccessibleInterval<R>> predictForInputTensors(List<Tensor<T>> inTensors) 
+	throws RunModelException {
+		if (!loaded)
+			throw new RuntimeException("Please load the model first.");
+		return executeCode(createInputsCode(inTensors));		
+	}
+	
+	private <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> 
+	Map<String, RandomAccessibleInterval<R>> executeCode(String code) 
+	throws RunModelException {
+		Map<String, RandomAccessibleInterval<R>> outMap;
+		try {
+			Task task = python.task(code);
+			task.waitFor();
+			if (task.status == TaskStatus.CANCELED)
+				throw new RuntimeException("Task canceled");
+			else if (task.status == TaskStatus.FAILED)
+				throw new RuntimeException(task.error);
+			else if (task.status == TaskStatus.CRASHED)
+				throw new RuntimeException(task.error);
+			loaded = true;
+			outMap = reconstructOutputs(task);
+			cleanShm();
+		} catch (IOException | InterruptedException e) {
+			try {
+				cleanShm();
+			} catch (InterruptedException | IOException e1) {
+				throw new RunModelException(Types.stackTrace(e1));
+			}
+			throw new RunModelException(Types.stackTrace(e));
+		}
+		return outMap;
+	}
+	
+	private <T extends RealType<T> & NativeType<T>> String 
+	createInpusCode(List<RandomAccessibleInterval<T>> rais) {
+		String code = "";
+		for (int i = 0; i < rais.size(); i ++) {
+			SharedMemoryArray shma = SharedMemoryArray.createSHMAFromRAI(rais.get(i));
+			code += codeToConvertShmaToPython(shma, "torch_rai_" + i);
+			inShmaList.add(shma);
+		}
+		code += OUTPUT_LIST_KEY + " = " + MODEL_VAR_NAME + "(";
+		for (int i = 0; i < rais.size(); i ++)
+			code += "torch_rai_" + i;
+		code = code.substring(0, code.length() - 2);
+		code += ")" + System.lineSeparator();
+		code += ""
+				+ SHMS_KEY + " = []" + System.lineSeparator()
+				+ SHM_NAMES_KEY + " = []" + System.lineSeparator()
+				+ DTYPES_KEY + " = []" + System.lineSeparator()
+				+ DIMS_KEY + " = []" + System.lineSeparator()
+				+ "globals()['" + SHMS_KEY + "'] = " + SHMS_KEY + System.lineSeparator();
+		code += "handle_output_list(OUTPUT_LIST_KEY)" + System.lineSeparator();
+		return code;
+	}
+	
+	/**
+	 * Simply run inference on the images provided. If the dimensions, number, data type or other
+	 * characteristic of the tensor is not correct, an exception will be thrown.
+	 * @param <T>
+	 * 	input data type
+	 * @param <R>
+	 * 	ouptut data type
+	 * @param inputs
+	 * 	the list of {@link RandomAccessibleInterval} that will be used as inputs
+	 * @return a list of {@link RandomAccessibleInterval} that has been outputed by the model
+	 * @throws RunModelException
+	 *             if there is an error in the execution of the model
+	 */
+	public <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>>
+	List<RandomAccessibleInterval<R>> inference(List<RandomAccessibleInterval<T>> inputs) throws RunModelException {
 
-	@Override
-	public <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> void run(
-			List<Tensor<T>> inTensors, List<Tensor<R>> outTensors) throws RunModelException {
+		if (!loaded)
+			throw new RuntimeException("Please load the model first.");
+		String code = createInpusCode(inputs);
+		Map<String, RandomAccessibleInterval<R>> map = executeCode(code);
+		List<RandomAccessibleInterval<R>> outRais = new ArrayList<RandomAccessibleInterval<R>>();
+		for (Entry<String, RandomAccessibleInterval<R>> ee : map.entrySet()) {
+			outRais.add(ee.getValue());
+		}
+		return outRais;
+	}
+	
+	private <T extends RealType<T> & NativeType<T>> String createInputsCode(List<Tensor<T>> inTensors) {
 		String code = "";
 		for (Tensor<T> in : inTensors) {
 			SharedMemoryArray shma = SharedMemoryArray.createSHMAFromRAI(in.getData());
 			code += codeToConvertShmaToPython(shma, in.getName() + "_torch");
 			inShmaList.add(shma);
 		}
-		for (Tensor<R> out : outTensors)
-			code += out.getName() + ", ";
-		code = code.substring(0, code.length() - 2);
-		code += " = " + MODEL_VAR_NAME + "(";
+		code += OUTPUT_LIST_KEY + " = " + MODEL_VAR_NAME + "(";
 		for (Tensor<T> in : inTensors)
-			code += in.getName() + ", ";
+			code += in.getName() + "_torch, ";
 		code = code.substring(0, code.length() - 2);
 		code += ")" + System.lineSeparator();
-		
+		code += ""
+				+ SHMS_KEY + " = []" + System.lineSeparator()
+				+ SHM_NAMES_KEY + " = []" + System.lineSeparator()
+				+ DTYPES_KEY + " = []" + System.lineSeparator()
+				+ DIMS_KEY + " = []" + System.lineSeparator()
+				+ "globals()['" + SHMS_KEY + "'] = " + SHMS_KEY + System.lineSeparator();
+		code += "handle_output_list(OUTPUT_LIST_KEY)" + System.lineSeparator();
+		return code;
 	}
 
 	@Override
-	public <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> List<Tensor<T>> run(
-			List<Tensor<R>> inputTensors) throws RunModelException {
-		// TODO Auto-generated method stub
-		return null;
+	public <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> 
+	List<Tensor<T>> run(List<Tensor<R>> inputTensors) throws RunModelException {
+		Map<String, RandomAccessibleInterval<T>> outMap = predictForInputTensors(inputTensors);
+		List<Tensor<T>> outTensors = new ArrayList<Tensor<T>>();
+		for (Entry<String, RandomAccessibleInterval<T>> ee : outMap.entrySet()) {
+			RandomAccessibleInterval<T> rai = ee.getValue();
+			Tensor<T> tt = Tensor.build(ee.getKey(), null, rai);
+			outTensors.add(tt);
+		}
+		return outTensors;
+	}
+
+	@Override
+	public <T extends RealType<T> & NativeType<T>, R extends RealType<R> & NativeType<R>> void run(
+			List<Tensor<T>> inTensors, List<Tensor<R>> outTensors) throws RunModelException {
+		Map<String, RandomAccessibleInterval<R>> outMap = predictForInputTensors(inTensors);
+		int c = 0;
+		for (Entry<String, RandomAccessibleInterval<R>> ee : outMap.entrySet()) {
+			RandomAccessibleInterval<R> rai = ee.getValue();
+			try {
+				outTensors.get(c).setData(rai);
+				c ++;
+			} catch (Exception ex) {
+			}
+			
+		}
+	}
+	
+	private void closeShm() throws IOException {
+		for (SharedMemoryArray shm : inShmaList) {
+			shm.close();
+		}
+	}
+	
+	private void cleanShm() throws InterruptedException, IOException {
+		closeShm();
+		if (PlatformDetection.isWindows()) {
+			Task closeSHMTask = python.task(CLEAN_SHM_CODE);
+			closeSHMTask.waitFor();
+		}
+	}
+	
+	protected <T extends RealType<T> & NativeType<T>> 
+	Map<String, RandomAccessibleInterval<T>> reconstructOutputs(Task task) throws IOException {
+		buildOutShmList(task);
+		buildOutDTypesList(task);
+		buildOutDimsList(task);
+		LinkedHashMap<String, RandomAccessibleInterval<T>> outs = new LinkedHashMap<String, RandomAccessibleInterval<T>>();
+		for (int i = 0; i < this.outShmNames.size(); i ++) {
+			String shmName = outShmNames.get(i);
+			String dtype = outShmDTypes.get(i);
+			long[] dims = outShmDims.get(i);
+			RandomAccessibleInterval<T> rai = reconstruct(shmName, dtype, dims);
+			outs.put("output_" + i, rai);
+		}
+		return outs;
+	}
+	
+	private void buildOutShmList(Task task) {
+		this.outShmNames = new ArrayList<String>();
+		if (task.outputs.get(SHM_NAMES_KEY) instanceof List == false)
+			throw new RuntimeException("Unexpected type for '" + SHM_NAMES_KEY + "'.");
+		List<?> list = (List<?>) task.outputs.get(SHM_NAMES_KEY);
+		for (Object elem : list) {
+			if (elem instanceof String == false)
+				throw new RuntimeException("Unexpected type for element of  '" + SHM_NAMES_KEY + "' list.");
+			outShmNames.add((String) elem);
+		}
+	}
+	
+	private void buildOutDTypesList(Task task) {
+		this.outShmDTypes = new ArrayList<String>();
+		if (task.outputs.get(DTYPES_KEY) instanceof List == false)
+			throw new RuntimeException("Unexpected type for '" + DTYPES_KEY + "'.");
+		List<?> list = (List<?>) task.outputs.get(DTYPES_KEY);
+		for (Object elem : list) {
+			if (elem instanceof String == false)
+				throw new RuntimeException("Unexpected type for element of  '" + DTYPES_KEY + "' list.");
+			outShmDTypes.add((String) elem);
+		}
+	}
+	
+	private void buildOutDimsList(Task task) {
+		this.outShmDims = new ArrayList<long[]>();
+		if (task.outputs.get(DIMS_KEY) instanceof List == false)
+			throw new RuntimeException("Unexpected type for '" + DIMS_KEY + "'.");
+		List<?> list = (List<?>) task.outputs.get(DIMS_KEY);
+		for (Object elem : list) {
+			if (elem instanceof Object[] == false)
+				throw new RuntimeException("Unexpected type for element of  '" + DIMS_KEY + "' list.");
+			Object[] arr = (Object[]) elem;
+			long[] longArr = new long[arr.length];
+			for (int i = 0; i < arr.length; i ++) {
+				if (arr[i] instanceof Number == false)
+					throw new RuntimeException("Unexpected type for array of element of  '" + DIMS_KEY + "' list.");
+				longArr[i] = ((Number) arr[i]).longValue();
+			}
+			outShmDims.add(longArr);
+		}
+	}
+	
+	private <T extends RealType<T> & NativeType<T>> 
+	RandomAccessibleInterval<T> reconstruct(String key, String dtype, long[] dims) throws IOException {
+
+		SharedMemoryArray shm = SharedMemoryArray.readOrCreate(key, dims, 
+				Cast.unchecked(CommonUtils.getImgLib2DataType(dtype)), false, false);
+		
+		// TODO I do not understand why is complaining when the types align perfectly
+		RandomAccessibleInterval<T> coordsRAI = shm.getSharedRAI();
+		RandomAccessibleInterval<T> coordsCopy = Tensor.createCopyOfRaiInWantedDataType(Cast.unchecked(coordsRAI), 
+				Util.getTypeFromInterval(Cast.unchecked(coordsRAI)));
+		
+		shm.close();
+		
+		return coordsCopy;
 	}
 	
 	public static String codeToConvertShmaToPython(SharedMemoryArray shma, String varName) {
@@ -208,6 +481,11 @@ public class DLModelPytorch extends BaseModel {
 			code += shma.getOriginalShape()[i] + ", ";
 		code += "])" + System.lineSeparator();
 		code += varName + " = torch.from_numpy(" + varName + "_np).to(" + MODEL_VAR_NAME + ".device)" + System.lineSeparator();
+
+		code += ""
+			+ "if os.name == 'nt':" + System.lineSeparator()
+			+ "  im_shm.close()" + System.lineSeparator()
+			+ "  im_shm.unlink()" + System.lineSeparator();
 		return code;
 	}
 	
