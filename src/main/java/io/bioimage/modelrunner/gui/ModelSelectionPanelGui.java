c6532358f1d5c95ad6d99fc2a675d998e122fb4b
carlosuc3m <100329787@alumnos.uc3m.es>
2025-04-24 18:30:04 +0200

    reorganize clases

--- src/main/java/io/bioimage/modelrunner/gui/ModelSelectionPanelGui.java
+++ src/main/java/io/bioimage/modelrunner/gui/ModelSelectionPanelGui.java
@@ -19,18 +19,18 @@ public class ModelSelectionPanelGui extends JPanel {
 
 	private static final long serialVersionUID = 6264134076603842497L;
 
-    private ModelCard prevModelPanel;
-    private ModelCard selectedModelPanel;
-    private ModelCard nextModelPanel;
+	protected ModelCard prevModelPanel;
+    protected ModelCard selectedModelPanel;
+    protected ModelCard nextModelPanel;
     
     protected JButton nextButton;
     protected JButton prevButton;
-    private TitledBorder lineBorder;
+    protected TitledBorder lineBorder;
     
-    private String defaultString = Gui.LOADING_STR;
+    protected String defaultString = Gui.LOADING_STR;
 
     protected static final double MAIN_CARD_RT = 1;
-    protected static final double SECOND_CARD_RT = 0.8;
+    protected static final double SECOND_CARD_RT = 0.6;
 
     protected static final double BTN_HEIGHT_RATIO = 0.07;
     protected static final double MAX_BTN_HEIGHT = 33;
@@ -70,29 +70,30 @@ public class ModelSelectionPanelGui extends JPanel {
             public void componentResized(ComponentEvent e) {
                 Insets in = getInsets();
                 int W = getWidth()  - in.left - in.right;
-                int H = getHeight() - in.top  - in.bottom;
+                int rawW = getWidth();
                 int rawH = getHeight();
-                int inset = 2;
+                int inset = in.left;
+                int insetTop = in.top;
                 
                 double btnH = Math.min(MAX_BTN_HEIGHT, rawH * BTN_HEIGHT_RATIO);
                 prevButton.setBounds(in.left, rawH - in.left - (int) btnH, (int) (W * 0.5), (int) btnH);
                 nextButton.setBounds(in.left + (int) (W * 0.5), rawH - in.left - (int) btnH, (int) (W * 0.5), (int) btnH);
                 
-                int hSide = (int) (H - inset * 2 - btnH);
+                int hSide = (int) (rawH - inset - insetTop - btnH);
                 int wSide = (int) ((W - 4 * inset) / (SECOND_CARD_RT * 2 + 1));
                 
                 int side = Math.min(wSide, hSide);
 
-                int mediumX = (int) (W / 2 - side / 2);
-                int leftX = (int) (mediumX - inset - side * (SECOND_CARD_RT + 0.5));
-                int rightX = (int) (mediumX + side / 2 + inset);
+                int mediumX = (int) (rawW / 2 - side / 2);
+                int leftX = (int) (mediumX - inset - side * SECOND_CARD_RT);
+                int rightX = (int) (mediumX + side + inset);
                 
-                int topY = (int) (H / 2 - side / 2);
+                int topY = (int) ((rawH - btnH) / 2 - side / 2);
                 int bottomY = (int) (topY + side * (1 - SECOND_CARD_RT) / 2);
                 
-                prevModelPanel.setBounds(leftX, bottomY, side, side);
+                prevModelPanel.setBounds(leftX, bottomY, (int) (side * SECOND_CARD_RT), (int) (side * SECOND_CARD_RT));
                 selectedModelPanel.setBounds(mediumX, topY, side, side);
-                nextModelPanel.setBounds(rightX, bottomY, side, side);
+                nextModelPanel.setBounds(rightX, bottomY, (int) (side * SECOND_CARD_RT), (int) (side * SECOND_CARD_RT));
             }
         });
     }
