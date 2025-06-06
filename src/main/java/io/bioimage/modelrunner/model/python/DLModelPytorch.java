481431d7be162b8b4aab61c6880c3a33fa95f828
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-06 14:16:12 +0200

    add javadoc

--- src/main/java/io/bioimage/modelrunner/model/python/DLModelPytorch.java
+++ src/main/java/io/bioimage/modelrunner/model/python/DLModelPytorch.java
@@ -26,6 +26,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class that contains the methods to use a Pytorch model from JDLL.
+ * The model should be compatible with the default environment (Biapy environment) or the environmemt
+ * preferred.
+ * 
+ * The callable to instantiate the model should be defined as well as the module from where it has to be imported
+ * or the Python file that contains its declaration.
+ * 
+ */
 public class DLModelPytorch extends DLModelPytorchProtected {
 	
 	protected DLModelPytorch(String modelFile, String callable, String importModule, String weightsPath, Map<String, Object> kwargs)
@@ -33,11 +42,41 @@ public class DLModelPytorch extends DLModelPytorchProtected {
 		super(modelFile, callable, importModule, weightsPath, kwargs);
 	}
 
+	/**
+	 * Create a Pytorch model that can run from Java
+	 * @param modelClass
+	 * 	file where the class of the model is defined. It the class can be imported this param can be null as long as the 
+	 * 	module from which the class can be imported is defined at 'importModule'
+	 * @param callable
+	 * 	class of the model
+	 * @param importModule
+	 * 	module from where the model class is imported. It can be null it we provide a file that contains the class
+	 * @param modelPath
+	 * 	path to the Pytorch weights
+	 * @return a Java class that can do inference using a Python Pytorch model
+	 * @throws IOException if there is any error trying to run Python from Java
+	 */
 	public static DLModelPytorch create(String modelClass, String callable, String importModule, 
 			String modelPath) throws IOException {
 		return create(modelClass, callable, importModule, modelPath, new HashMap<String, Object>());
 	}
-	
+
+	/**
+	 * Create a Pytorch model that can run from Java
+	 * @param modelClass
+	 * 	file where the class of the model is defined. It the class can be imported this param can be null as long as the 
+	 * 	module from which the class can be imported is defined at 'importModule'
+	 * @param callable
+	 * 	class of the model
+	 * @param importModule
+	 * 	module from where the model class is imported. It can be null it we provide a file that contains the class
+	 * @param modelPath
+	 * 	path to the Pytorch weights
+	 * @param kwargs
+	 * 	other kwargs that might be needed to instantiate the model
+	 * @return a Java class that can do inference using a Python Pytorch model
+	 * @throws IOException if there is any error trying to run Python from Java
+	 */
 	public static DLModelPytorch create(String modelClass, String callable, String importModule, 
 			String modelPath, Map<String, Object> kwargs) throws IOException {
 		return new DLModelPytorch(modelClass, callable, importModule, modelPath, kwargs);
